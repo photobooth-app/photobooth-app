95710ed7e99fafce54b94aa0bd1d0feb1e4bd2c6
Michael <michael@groene.de>
2022-11-05 20:02:41 +0100

    services updated

---
+++ boothupload.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#setup rclone with "rclone config" first! use "boothupload" as remote name
+
+SOURCE_DIR="/var/www/html/photobooth/data/images/"
+DEST_DIR="boothupload:/"
+
+#wait until wifi established for init sync.
+sleep 10
+
+while [[ true ]] ; do
+	rclone sync --progress $SOURCE_DIR $DEST_DIR
+	sleep 1
+	inotifywait --timeout 300 -e modify,delete,create,move $SOURCE_DIR
+done
