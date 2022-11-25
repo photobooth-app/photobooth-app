9afc4451df200c91210778b56d34b93cb8ddcd31
Michael <michael@groene.de>
2022-11-25 10:05:55 +0100

    added start script

---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# needs sudo rights
+
+# remove cache files
+rm -r ./__pycache__ 2> /dev/null
+rm -r ./lib/__pycache__ 2> /dev/null
+rm -r ./utils/__pycache__ 2> /dev/null
+
+# start application
+python ./imageserver.py
