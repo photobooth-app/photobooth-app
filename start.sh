26b9e7bde2df8c015ed37e4d5b18565391395245
Michael <michael@groene.de>
2022-11-29 15:14:25 +0100

    start script change workdirectory to where it lives

--- start.sh
+++ start.sh
@@ -2,6 +2,10 @@
 
 # needs sudo rights
 
+# change working directory to where this script lives in https://stackoverflow.com/questions/6393551/what-is-the-meaning-of-0-in-a-bash-script
+cd "${0%/*}"
+
+
 # remove cache files
 rm -r ./__pycache__ 2> /dev/null
 rm -r ./lib/__pycache__ 2> /dev/null
