e8085f03fb08ec5d43b8462a73a6a56896785e21
Michael <michael@groene.de>
2022-12-11 13:28:27 +0100

    finally changed to keyboard library. works in service as well as in start.sh well. limitation: needs sudo.

--- lib/KeyboardService.py
+++ lib/KeyboardService.py
@@ -1,39 +1,20 @@
 import logging
-import asyncio
-from sshkeyboard import listen_keyboard
-import threading
+import keyboard
 
-#from pynput import keyboard
 logger = logging.getLogger(__name__)
 
 
-class KeyboardService(threading.Thread):
+class KeyboardService():
 
-    def __init__(self, cs, ee, name='keyboard-input-thread'):
+    def __init__(self, cs, ee):
         self._cs = cs
         self._ee = ee
 
-        self._stop = threading.Event()
+        keyboard.on_press(self.on_key_callback)
 
-        super(KeyboardService, self).__init__(name=name)
-        self.start()
+    def on_key_callback(self, key):
+        logger.debug(f"key '{key.name}' triggered.")
 
-    # function using _stop function
-    def stop(self):
-        self._stop.set()
-
-    def stopped(self):
-        return self._stop.isSet()
-
-    def run(self):
-        while not self.stopped():
-            listen_keyboard(
-                on_press=self.on_key_callback, until=None, lower=False
-            )
-
-    async def on_key_callback(self, key):
-        logger.debug(f"key '{key}' triggered.")
-
-        if key == self._cs._current_config["HW_KEYCODE_TAKEPIC"]:
+        if key.name == self._cs._current_config["HW_KEYCODE_TAKEPIC"]:
             logger.info("triggered by keyboard input HW_KEYCODE_TAKEPIC")
             self._ee.emit("triggerprocess/chose_1pic")
