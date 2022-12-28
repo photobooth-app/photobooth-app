e84bccd9279692e9680daa46d76b4c49eafbd3a7
Michael G <michael@groene.de>
2022-12-28 23:30:41 +0100

    exif service adjusted to imageserver

--- lib/Exif.py
+++ lib/Exif.py
@@ -1,26 +1,29 @@
 import piexif
 import datetime
 import logging
+from lib.ImageServerAbstract import ImageServerAbstract
+from lib.LocationService import LocationService
 logger = logging.getLogger(__name__)
 
 
 class Exif():
     """Handle all image related stuff"""
 
-    def __init__(self, frameServer, locationservice):
-        self._frameServer = frameServer
+    def __init__(self, imageServer: ImageServerAbstract, locationservice: LocationService):
+        self._imageServer = imageServer
         self._locationservice = locationservice
 
     def createExifBytes(self):
+        print(self._imageServer.metadata)
         # grab metadata from frameserver
         now = datetime.datetime.now()
-        zero_ifd = {piexif.ImageIFD.Make: self._frameServer.exif_make,
+        zero_ifd = {piexif.ImageIFD.Make: self._imageServer.exif_make,
                     # self._frameServer._picam2.camera.id,
-                    piexif.ImageIFD.Model: self._frameServer.exif_model,
+                    piexif.ImageIFD.Model: self._imageServer.exif_model,
                     piexif.ImageIFD.Software: "Photobooth Imageserver"}
-        total_gain = getattr(self._frameServer, "metadata.AnalogueGain", 0) * \
-            getattr(self._frameServer, "metadata.DigitalGain", 0)
-        exif_ifd = {piexif.ExifIFD.ExposureTime: (getattr(self._frameServer, "metadata.ExposureTime", 0), 1000000),
+        total_gain = (self._imageServer.metadata.get("AnalogueGain", 0) *
+                      self._imageServer.metadata.get("DigitalGain", 0))
+        exif_ifd = {piexif.ExifIFD.ExposureTime: (self._imageServer.metadata.get("ExposureTime", 0), 1000000),
                     piexif.ExifIFD.DateTimeOriginal: now.strftime("%Y:%m:%d %H:%M:%S"),
                     piexif.ExifIFD.ISOSpeedRatings: int(total_gain * 100)}
 
@@ -39,7 +42,7 @@ class Exif():
             }
             # add gps dict
             exif_dict.update({"GPS": gps_ifd})
-
+        logger.info(f"gathered following exif data to be injected {exif_dict}")
         exif_bytes = piexif.dump(exif_dict)
 
         return exif_bytes
