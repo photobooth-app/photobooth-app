bd315b71a4bc77b65e8e28e0b3e9c801f38c09f8
Michael G <michael@groene.de>
2022-12-28 23:30:06 +0100

    autofocus refactored

--- lib/Autofocus.py
+++ lib/Autofocus.py
@@ -12,6 +12,52 @@ from lib.RepeatedTimer import RepeatedTimer
 logger = logging.getLogger(__name__)
 
 
+class Focuser:
+    """
+    ###
+    # This script works after installing the driver for 16mp imx519 driver from arducam
+    # only driver necessary, not the libcamera apps
+    # How to install the driver
+    # https://www.arducam.com/docs/cameras-for-raspberry-pi/raspberry-pi-libcamera-guide/how-to-use-arducam-16mp-camera-on-rapberry-pi/
+    # You can use our auto-install script to install the driver for arducam 64MP camera:
+    # wget - O install_pivariety_pkgs.sh https: // github.com/ArduCAM/Arducam-Pivariety-V4L2-Driver/releases/download/install_script/install_pivariety_pkgs.sh
+    # chmod + x install_pivariety_pkgs.sh
+    # ./install_pivariety_pkgs.sh - p imx519_kernel_driver_low_speed
+    #
+    # driver may have to be reinstalled after system updates!
+    """
+
+    def __init__(self):
+        self.focus_value = 0
+        self._device = settings.common.FOCUSER_DEVICE
+        self.MAX_VALUE = settings.common.FOCUSER_MAX_VALUE
+        self.MIN_VALUE = settings.common.FOCUSER_MIN_VALUE
+
+        self.reset()
+
+    def reset(self):
+        self.set(settings.common.FOCUSER_DEF_VALUE)
+
+    def get(self):
+        return self.focus_value
+
+    def set(self, value):
+        if value > settings.common.FOCUSER_MAX_VALUE:
+            value = settings.common.FOCUSER_MAX_VALUE
+        elif value < settings.common.FOCUSER_MIN_VALUE:
+            value = settings.common.FOCUSER_MIN_VALUE
+
+        value = int(value)
+        try:
+            os.system(
+                "v4l2-ctl -c focus_absolute={} -d {}".format(value, self._device))
+        except Exception as e:
+            logger.exception(e)
+            logger.error(f"Error on focus command: {e}")
+
+        self.focus_value = value
+
+
 class FocusState(object):
     def __init__(self, imageServer, ee):
         self._imageServer: ImageServerAbstract = imageServer
@@ -96,7 +142,7 @@ def getROIFrame(roi, frame):
     return roi_frame
 
 
-def statsThread(imageServer, focuser, focusState):
+def statsThread(imageServer: ImageServerAbstract, focuser: Focuser, focusState: FocusState):
     maxPosition = focuser.MAX_VALUE
     minPosition = focuser.MIN_VALUE
     lastPosition = focuser.get()
@@ -107,8 +153,14 @@ def statsThread(imageServer, focuser, focusState):
     sharpnessList = []
 
     while not focusState.isFinish():
-
-        frame = imageServer._wait_for_lores_frame()
+        try:
+            frame = imageServer.wait_for_lores_frame()
+        except NotImplementedError as e:
+            logger.error(
+                f"imageserver backend not to deliver lores frames for autofocus - please disable autofocus")
+            focusState.stopRegularAutofocusTimer()
+            focusState.reset()
+            break
 
         if time.time() - lastTime >= settings.common.FOCUSER_MOVE_TIME and not focusState.isFinish():
             lastTime = time.time()
@@ -196,54 +248,3 @@ def focusThread(focuser, focusState):
         focuser.set(maxItem[0])
     else:
         focuser.set(maxPosition)
-
-
-class Focuser:
-    """
-    ###
-    # This script works after installing the driver for 16mp imx519 driver from arducam
-    # only driver necessary, not the libcamera apps
-    # How to install the driver
-    # https://www.arducam.com/docs/cameras-for-raspberry-pi/raspberry-pi-libcamera-guide/how-to-use-arducam-16mp-camera-on-rapberry-pi/
-    # You can use our auto-install script to install the driver for arducam 64MP camera:
-    # wget - O install_pivariety_pkgs.sh https: // github.com/ArduCAM/Arducam-Pivariety-V4L2-Driver/releases/download/install_script/install_pivariety_pkgs.sh
-    # chmod + x install_pivariety_pkgs.sh
-    # ./install_pivariety_pkgs.sh - p imx519_kernel_driver_low_speed
-    #
-    # driver may have to be reinstalled after system updates!
-    """
-
-    def __init__(self):
-        self.focus_value = 0
-        self._device = settings.common.FOCUSER_DEVICE
-        self.MAX_VALUE = settings.common.FOCUSER_MAX_VALUE
-        self.MIN_VALUE = settings.common.FOCUSER_MIN_VALUE
-
-        # if arducam drivers are not installed properly, the device might not exist and we have to fail hard here now.
-        if not (file_exists(self._device)):
-            raise Exception(
-                f"ERROR! Focuser device {self._device} not existing!")
-
-        self.reset()
-
-    def reset(self):
-        self.set(settings.common.FOCUSER_DEF_VALUE)
-
-    def get(self):
-        return self.focus_value
-
-    def set(self, value):
-        if value > settings.common.FOCUSER_MAX_VALUE:
-            value = settings.common.FOCUSER_MAX_VALUE
-        elif value < settings.common.FOCUSER_MIN_VALUE:
-            value = settings.common.FOCUSER_MIN_VALUE
-
-        value = int(value)
-        try:
-            os.system(
-                "v4l2-ctl -c focus_absolute={} -d {}".format(value, self._device))
-        except Exception as e:
-            logger.exception(e)
-            logger.error(f"Error on focus command: {e}")
-
-        self.focus_value = value
