c2ba5c39ab9c191a88794f759119289dc08e165e
michael <me@mgrl.de>
2023-07-22 09:44:16 +0200

    update php testing

--- tests/test_main.py
+++ tests/test_main.py
@@ -9,6 +9,7 @@ import pytest
 logger = logging.getLogger(name=None)
 
 
+# TODO: this modifies the main variable -> tests stop logging afterwards due to shutdown ressources
 def test_main_package():
     from photobooth import __main__
 
