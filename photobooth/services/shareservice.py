26d276737454a1721c2b22649fc66922d4d53c24
michael <me@mgrl.de>
2023-07-23 18:02:53 +0200

    improved error handling

--- photobooth/services/shareservice.py
+++ photobooth/services/shareservice.py
@@ -34,18 +34,22 @@ class ShareService(BaseService):
         self._initialized = False
 
         self._logger.info("checking shareservice api endpoint")
-        r = requests.get(self._config.common.shareservice_url, params={"action": "info"})
-        if r.status_code == 200:
-            try:
-                info = r.json()
-                self._logger.info(f"{info=}")
-            except requests.exceptions.JSONDecodeError as exc:
-                self._logger.error(f"api endpoint error: {exc}")
-
-            self._initialized = True
-            self._logger.info("api endpoint found, URL valid")
+        try:
+            r = requests.get(self._config.common.shareservice_url, params={"action": "info"}, timeout=10)
+        except Exception as exc:
+            self._logger.warning(f"error checking shareservice api endpoint: {exc}")
         else:
-            self._logger.error("api endpoint check failed")
+            if r.status_code == 200:
+                try:
+                    info = r.json()
+                    self._logger.info(f"{info=}")
+                except requests.exceptions.JSONDecodeError as exc:
+                    self._logger.error(f"api endpoint error: {exc}")
+
+                self._initialized = True
+                self._logger.info("api endpoint found, URL valid")
+            else:
+                self._logger.error("api endpoint check failed")
 
     def start(self):
         """_summary_"""
@@ -70,7 +74,7 @@ class ShareService(BaseService):
 
         while not self._worker_thread.stopped():
             payload = {"action": "upload_queue"}
-            r = requests.get(self._config.common.shareservice_url, params=payload, stream=True, timeout=60)
+            r = requests.get(self._config.common.shareservice_url, params=payload, stream=True, timeout=8)
             if r.encoding is None:
                 r.encoding = "utf-8"
 
@@ -79,11 +83,14 @@ class ShareService(BaseService):
             else:
                 self._logger.error("problem connecting to shareservice dl.php script!")
 
-            iterator = r.iter_lines(decode_unicode=True)
+            iterator = r.iter_lines(chunk_size=40, decode_unicode=True)
 
             while not self._worker_thread.stopped():
                 try:
                     line = next(iterator)
+                except StopIteration:
+                    self._logger.debug("dl.php script finished after some time. stopiteration issued-reconnect")
+                    break
                 except Exception as exc:
                     self._logger.warning(f"encountered shareservice connection issue. retrying. error: {exc}")
                     break
@@ -132,20 +139,18 @@ class ShareService(BaseService):
                             self._logger.debug(f"response from php server: {r.text}")
                             self._logger.debug(f"-- request took: {round((time.time() - start_time), 2)}s")
                     elif decoded_line.get("ping", None):
-                        if self._worker_thread.stopped():
-                            self._logger.debug("stop workerthread requested")
-                            break
+                        pass
                     else:
                         self._logger.error(f"invalid queue line, ignore: {line}")
-                else:
-                    # if a keepalive message is issued, we can check here also regularly for exit condition set
-                    if self._worker_thread.stopped():
-                        self._logger.debug("stop workerthread requested")
-                        break
+
+                # if a keepalive message is issued, we can check here also regularly for exit condition set
+                if self._worker_thread.stopped():
+                    self._logger.debug("stop workerthread requested")
+                    break
 
             self._logger.info("request timed out, error occured or shutdown requested")
             if not self._worker_thread.stopped():
-                self._logger.info("restarting loop after 5 seconds")
-                time.sleep(5)
+                self._logger.info("restarting loop wait 1 second")
+                time.sleep(1)
 
         self._logger.info("leaving shareservice workerthread")
