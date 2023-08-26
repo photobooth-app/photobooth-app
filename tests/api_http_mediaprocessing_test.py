d9a12ec254377fd793cff1dd68236df151a6cc29
michael <me@mgrl.de>
2023-08-26 08:07:19 +0200

    fix test: using correct container instance and close PIL image to avoid stalled pytest after successful tests

--- tests/api_http_mediaprocessing_test.py
+++ tests/api_http_mediaprocessing_test.py
@@ -1,15 +1,14 @@
 import io
+import logging
 
 import pytest
-from dependency_injector import providers
 from fastapi.testclient import TestClient
 from PIL import Image
-from pymitter import EventEmitter
 
-from photobooth.appconfig import AppConfig
-from photobooth.application import app
-from photobooth.services.backends.containers import BackendsContainer
+from photobooth.application import ApplicationContainer, app
 from photobooth.services.containers import ServicesContainer
+
+logger = logging.getLogger(name=None)
 
 
 @pytest.fixture
@@ -19,30 +18,18 @@ def client() -> TestClient:
 
 
 # need fixture on module scope otherwise tests fail because GPIO lib gets messed up
-@pytest.fixture(scope="module")
+@pytest.fixture()
 def services() -> ServicesContainer:
-    # setup
-    evtbus = providers.Singleton(EventEmitter)
-    config = providers.Singleton(AppConfig)
-    services = ServicesContainer(
-        evtbus=evtbus,
-        config=config,
-        backends=BackendsContainer(
-            evtbus=evtbus,
-            config=config,
-        ),
-    )
-
-    services.init_resources()
+    app_container: ApplicationContainer = app.container
 
     # create one image to ensure there is at least one
-    services.processing_service().shoot()
-    services.processing_service().postprocess()
-    services.processing_service().finalize()
+    app_container.services().processing_service().shoot()
+    app_container.services().processing_service().postprocess()
+    app_container.services().processing_service().finalize()
 
     # deliver
-    yield services
-    services.shutdown_resources()
+    yield app_container.services
+    app_container.services().shutdown_resources()
 
 
 def test_preview_filter_original(client: TestClient, services: ServicesContainer):
@@ -102,10 +89,21 @@ def test_apply_filter_original(client: TestClient, services: ServicesContainer):
     # get the newest mediaitem
     mediaitem = services.mediacollection_service().db_get_images()[0]
 
-    image_before = Image.open(mediaitem.path_full)
+    response = client.get(f"/mediaprocessing/applyfilter/{mediaitem.id}/original")
+    assert response.status_code == 200
+
+    image_original = Image.open(mediaitem.path_preview).copy()
+
+    response = client.get(f"/mediaprocessing/applyfilter/{mediaitem.id}/_1977")
+    assert response.status_code == 200
+    if list(image_original.getdata()) == list(Image.open(mediaitem.path_preview).getdata()):
+        raise AssertionError("img data before and after same. filter was not applied!")
 
     response = client.get(f"/mediaprocessing/applyfilter/{mediaitem.id}/original")
 
     assert response.status_code == 200
 
-    assert list(image_before.getdata()) == list(Image.open(mediaitem.path_full).getdata())
+    assert list(image_original.getdata()) == list(Image.open(mediaitem.path_preview).getdata())
+
+    # need to close image, otherwise pytest hangs!
+    image_original.close()
