b090ff72b57c3a7f5f391738d31d55b7261763e6
michael <me@mgrl.de>
2023-10-31 09:06:20 +0100

    improve test coverage

---
+++ tests/test_application.py
@@ -0,0 +1,42 @@
+"""
+Testing Simulated Backend
+"""
+import json
+import logging
+import os
+from unittest.mock import patch
+
+import pytest
+from fastapi.testclient import TestClient
+
+from photobooth.appconfig import AppConfig
+
+logger = logging.getLogger(name=None)
+
+
+def test_app_runtime_exc_folder_creation_failed():
+    with patch.object(os, "makedirs", side_effect=OSError("test")):
+        # emulate write access issue and ensure an exception is received to make the app fail starting.
+        with pytest.raises(OSError):
+            from photobooth.application import app
+
+            _ = app
+
+
+@pytest.fixture
+def client() -> TestClient:
+    from photobooth.application import app
+
+    with TestClient(app=app, base_url="http://test") as client:
+        yield client
+        client.app.container.shutdown_resources()
+
+
+def test_config_post_validationerror(client: TestClient):
+    config_dict = json.loads(AppConfig().model_dump_json())
+    # set illegal setting, that provokes validation error
+    config_dict["common"]["countdown_capture_first"] = 30
+
+    response = client.post("/admin/config/current", json=config_dict)
+
+    assert response.status_code == 422
--- tests/test_router_config.py
+++ tests/test_router_config.py
@@ -35,7 +35,7 @@ def test_config_endpoints(client: TestClient, config_endpoint):
 
 def test_config_post(client: TestClient):
     # jsonify using pydantic's json function, because fastapi cannot convert all types (like Color)
-    config_dict = {"updated_settings": json.loads(AppConfig().model_dump_json())}
+    config_dict = json.loads(AppConfig().model_dump_json())
 
     response = client.post("/admin/config/current", json=config_dict)
     assert response.status_code == 200
