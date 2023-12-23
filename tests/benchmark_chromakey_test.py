78def8128a49766185278369ddfb831e45d2e145
michael <me@mgrl.de>
2023-12-23 09:42:25 +0100

    added simpler chromakey benchmark test for live stream processing. still not fast enough to implement.

--- tests/benchmark_chromakey_test.py
+++ tests/benchmark_chromakey_test.py
@@ -107,7 +107,48 @@ def opencv_chromakey(pil_image: Image):
     return convert_from_cv2_to_image(result)
 
 
-@pytest.fixture(params=["pil_chromakey", "opencv_chromakey"])
+def opencv_chromakey_live(pil_image: Image):
+    # reduced quality optimized for higher speed.
+
+    BLUR_SIZE = 2
+    DILATE_SIZE = 4
+
+    def convert_from_cv2_to_image(img: np.ndarray) -> Image:
+        return Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGRA2RGBA))
+
+    def convert_from_image_to_cv2(img: Image) -> np.ndarray:
+        return cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
+
+    # choose hsv parameters: https://docs.opencv.org/4.x/df/d9d/tutorial_py_colorspaces.html
+    # https://stackoverflow.com/questions/10948589/choosing-the-correct-upper-and-lower-hsv-boundaries-for-color-detection-withcv/48367205#48367205
+    # https://stackoverflow.com/questions/48109650/how-to-detect-two-different-colors-using-cv2-inrange-in-python-opencv
+    # https://www.geeksforgeeks.org/opencv-invert-mask/
+    # https://stackoverflow.com/questions/51719472/remove-green-background-screen-from-image-using-opencv-python
+    # https://docs.opencv.org/3.4/d9/d61/tutorial_py_morphological_ops.html
+
+    frame = convert_from_image_to_cv2(pil_image)
+    ## convert to hsv
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    # mask of green
+    mask = cv2.inRange(hsv, np.array(GREEN_RANGE_MIN_HSV), np.array(GREEN_RANGE_MAX_HSV))
+
+    # Inverting the mask
+    mask_inverted = cv2.bitwise_not(mask)
+
+    # actually remove the background (so if transparency is ignored later in processing,
+    # the removed parts are black instead just return)
+    result = cv2.bitwise_and(frame, frame, mask=mask_inverted)
+    # create result with transparent channel
+    result = cv2.cvtColor(result, cv2.COLOR_BGR2BGRA)
+    result[:, :, 3] = mask_inverted  # add mask to image as alpha channel
+
+    # cv2.imshow("Input", result)
+    # cv2.waitKey(0)
+    return convert_from_cv2_to_image(result)
+
+
+@pytest.fixture(params=["pil_chromakey", "opencv_chromakey", "opencv_chromakey_live"])
 def library(request):
     # yield fixture instead return to allow for cleanup:
     yield request.param
