photobooth-app/photobooth-app

commit 018c5517858c9ae0909d1aa84aab20bc7335795d

backend improve handling to recover from failed backends but keep stream upright.

photobooth/services/backends/abstractbackend.py
@@ -5,8 +5,11 @@ import dataclasses
 import logging
 import time
 from abc import ABC, abstractmethod
+from io import BytesIO
 from multiprocessing import Condition, Lock, shared_memory
+from pathlib import Path
 
+from PIL import Image, ImageDraw, ImageFont, ImageOps
 from pymitter import EventEmitter
 
 from ...appconfig import AppConfig
@@ -121,6 +124,27 @@ class AbstractBackend(ABC):
     # FUNCTIONS IMPLEMENTED IN ABSTRACT CLASS
     #
 
+    def _error_image(self, error_message: str) -> bytes:
+        path_font = Path(__file__).parent.joinpath("assets", "backend_simulated", "fonts", "Roboto-Bold.ttf").resolve()
+        text_fill = "#888"
+
+        img = Image.new("RGB", (400, 300), "#ddd")
+        img_draw = ImageDraw.Draw(img)
+        font_large = ImageFont.truetype(font=str(path_font), size=22)
+        font_small = ImageFont.truetype(font=str(path_font), size=15)
+        img_draw.text((25, 100), "Oh no - stream error :(", fill=text_fill, font=font_large)
+        img_draw.text((25, 120), f"{error_message}", fill=text_fill, font=font_small)
+        img_draw.text((25, 140), "please check camera and logs", fill=text_fill, font=font_small)
+
+        # flip if mirror effect is on because messages shall be readable on screen
+        if self._config.uisettings.livestream_mirror_effect:
+            img = ImageOps.mirror(img)
+
+        # create jpeg
+        jpeg_buffer = BytesIO()
+        img.save(jpeg_buffer, format="jpeg", quality=80)
+        return jpeg_buffer.getvalue()
+
     def gen_stream(self):
         """
         yield jpeg images to stream to client (if not created otherwise)
@@ -129,40 +153,36 @@ class AbstractBackend(ABC):
         """
         logger.info(f"livestream started on backend {self=}")
 
-        # lores stream attempts is doubled so if still fails after retry_capture times gen_stream did not fail yet
-        stream_max_attempts = self._config.backends.retry_capture * 2
-
         last_time = time.time_ns()
         while True:
-            for attempt in range(1, stream_max_attempts + 1):
-                try:
-                    buffer = self._wait_for_lores_image()
-                except TimeoutError:
-                    logger.error("error capture lores image for stream. " f"timeout expired {attempt=}/{stream_max_attempts}, retrying")
-                    # can we do additional error handling here?
-                except ShutdownInProcessError:
-                    logger.warning("gather img failed due to resources shutting down")
-                else:
-                    break
-            else:
-                # we failed finally all the attempts - deal with the consequences.
-                logger.critical("critical error getting stream. " f"failed to get lores image after {stream_max_attempts} attempts. giving up!")
-
-                # return to signal stop yielding frames to calling function
+            try:
+                output_jpeg_bytes = self._wait_for_lores_image()
+            except TimeoutError:
+                logger.error("error capture lores image for stream. timeout expired, retrying")
+                # can we do additional error handling here?
+                output_jpeg_bytes = self._error_image("timeout error")
+            except ShutdownInProcessError:
+                logger.warning("gather img failed due to resources shutting down")
                 return
+            except Exception as exc:
+                logger.error(f"streaming exception: {exc}")
+                output_jpeg_bytes = self._error_image(exc)
 
             now_time = time.time_ns()
             if (now_time - last_time) / 1000**3 >= (1 / self._config.backends.LIVEPREVIEW_FRAMERATE):
                 last_time = now_time
 
                 try:
-                    yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + buffer + b"\r\n\r\n")
+                    yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + output_jpeg_bytes + b"\r\n\r\n")
 
                 except GeneratorExit:
-                    # TODO: this is not triggered unfortunately. could be useful for cleanup if no stream is
+                    # TODO: this is not triggered unfortunately if client exits.
+                    # could be useful for cleanup if no stream is
                     # requested any more. otherwise consider delete
                     logger.debug(f"request exit! {self=}")
                     break
+                except Exception as exc:
+                    logger.error(f"error streaming {exc}")
 
 
 #

photobooth/services/backends/gphoto2.py
@@ -18,6 +18,7 @@ from photobooth.services.backends.abstractbackend import AbstractBackend, Backen
 from photobooth.utils.stoppablethread import StoppableThread
 
 from ...appconfig import AppConfig
+from ...utils.exceptions import ShutdownInProcessError
 
 logger = logging.getLogger(__name__)
 
@@ -63,7 +64,6 @@ class Gphoto2Backend(AbstractBackend):
         )
 
         self._camera_connected = False
-        self._camera_preview_available = False
 
         # worker threads
         self._worker_thread: StoppableThread = None
@@ -138,30 +138,21 @@ class Gphoto2Backend(AbstractBackend):
 
     def _check_camera_preview_available(self):
         """Test on init whether preview is available for this camera."""
-        preview_available = False
         try:
             self._camera.capture_preview()
         except Exception as exc:
             logger.info(f"gather preview failed; disabling preview in this session. consider to disable permanently! {exc}")
         else:
-            preview_available = True
-
-        return preview_available
+            logger.info("preview is available")
 
     def _wait_for_lores_image(self):
         """for other threads to receive a lores JPEG image"""
-        if self._config.backends.LIVEPREVIEW_ENABLED and not self._camera_preview_available:
-            raise RuntimeError(
-                "Camera cannot deliver preview stream, capture preview is disabled in this session. "
-                "Consider disable livestream on gphoto2 backend permanently."
-            )
-
         if self._worker_thread.stopped():
-            raise RuntimeError("shutdown already in progress, abort early")
+            raise ShutdownInProcessError("shutdown already in progress, abort early")
 
         with self._lores_data.condition:
             if not self._lores_data.condition.wait(timeout=4):
-                raise TimeoutError("timeout receiving frames")
+                raise TimeoutError("timeout receiving preview from DSLR")
             return self._lores_data.data
 
     def _wait_for_lores_frame(self):
@@ -239,7 +230,7 @@ class Gphoto2Backend(AbstractBackend):
                 logger.debug(f"{self.__module__} started")
 
             if self._config.backends.LIVEPREVIEW_ENABLED:
-                self._camera_preview_available = self._check_camera_preview_available()
+                self._check_camera_preview_available()
 
             self._worker_thread = StoppableThread(name="gphoto2_worker_thread", target=self._worker_fun, daemon=True)
             self._worker_thread.start()
@@ -258,7 +249,7 @@ class Gphoto2Backend(AbstractBackend):
 
         while not self._worker_thread.stopped():  # repeat until stopped
             if not self._hires_data.request_ready.is_set():
-                if self._config.backends.LIVEPREVIEW_ENABLED and self._camera_preview_available:
+                if self._config.backends.LIVEPREVIEW_ENABLED:
                     try:
                         capture = self._camera.capture_preview()
                         preview_failcounter = 0
@@ -266,7 +257,7 @@ class Gphoto2Backend(AbstractBackend):
                         preview_failcounter += 1
 
                         if preview_failcounter <= 10:
-                            logger.warning(f"error capturing frame despite general availability. {exc}")
+                            logger.warning(f"error capturing frame from DSLR: {exc}")
                             # abort this loop iteration and continue sleeping...
                             time.sleep(0.5)  # add another delay to avoid flooding logs
 

photobooth/services/backends/picamera2_.py
@@ -13,6 +13,7 @@ from pymitter import EventEmitter
 from photobooth.utils.stoppablethread import StoppableThread
 
 from ...appconfig import AppConfig, EnumFocuserModule
+from ...utils.exceptions import ShutdownInProcessError
 from .abstractbackend import AbstractBackend, BackendStats
 from .picamera2_libcamafcontinuous import Picamera2LibcamAfContinuous
 from .picamera2_libcamafinterval import Picamera2LibcamAfInterval
@@ -287,7 +288,7 @@ class Picamera2Backend(AbstractBackend):
     def _wait_for_lores_image(self):
         """for other threads to receive a lores JPEG image"""
         if self._generate_images_thread.stopped():
-            raise RuntimeError("shutdown already in progress, abort early")
+            raise ShutdownInProcessError("shutdown already in progress, abort early")
 
         with self._lores_data.condition:
             if not self._lores_data.condition.wait(timeout=4):

photobooth/services/backends/simulated.py
@@ -10,7 +10,7 @@ from io import BytesIO
 from multiprocessing import Condition, Event, Lock, Process, shared_memory
 from pathlib import Path
 
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image, ImageDraw, ImageFont, ImageOps
 from pymitter import EventEmitter
 
 from ...appconfig import AppConfig
@@ -62,6 +62,7 @@ class SimulatedBackend(AbstractBackend):
                 self._condition_img_buffer_ready,
                 self._img_buffer_lock,
                 self._event_proc_shutdown,
+                self._config.uisettings.livestream_mirror_effect,
             ),
             daemon=True,
         )
@@ -163,6 +164,7 @@ def img_aquisition(
     _condition_img_buffer_ready: Condition,
     _img_buffer_lock: Lock,
     _event_proc_shutdown: Event,
+    _mirror: bool,
 ):
     """function started in separate process to deliver images"""
 
@@ -217,9 +219,15 @@ def img_aquisition(
             font=font_small,
         )
 
+        # flip if mirror effect is on because messages shall be readable on screen
+        if _mirror:
+            img = ImageOps.mirror(img)
+
         # create jpeg
         jpeg_buffer = BytesIO()
         img.save(jpeg_buffer, format="jpeg", quality=90)
+
+        # TODO: remove time.sleep(random.randrange(2, 6))
 
         # put jpeg on queue until full. If full this function blocks until queue empty
         with _img_buffer_lock:

commit 07b191398378da4bc042573aacceeba51d9c3b71

fix high cpu load issue introduced in 018c5517858c9ae0909d1aa84aab20bc7335795d

photobooth/services/backends/abstractbackend.py
@@ -52,7 +52,7 @@ class AbstractBackend(ABC):
             backend_name=self.__class__.__name__,
         )
         self._fps = 0
-        self._stats_thread = StoppableThread(name="_statsThread", target=self._stats_fun, daemon=True)
+        self._stats_thread: StoppableThread = None
 
         super().__init__()
 
@@ -87,13 +87,15 @@ class AbstractBackend(ABC):
     @abstractmethod
     def start(self):
         """To start the backend to serve"""
+        self._stats_thread = StoppableThread(name="_statsThread", target=self._stats_fun, daemon=True)
         self._stats_thread.start()
 
     @abstractmethod
     def stop(self):
         """To stop the backend to serve"""
-        self._stats_thread.stop()
-        self._stats_thread.join()
+        if self._stats_thread:
+            self._stats_thread.stop()
+            self._stats_thread.join()
 
     #
     # INTERNAL FUNCTIONS TO BE IMPLEMENTED
@@ -216,6 +218,9 @@ class AbstractBackend(ABC):
                     )
 
                 yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + output_jpeg_bytes + b"\r\n\r\n")
+
+            # sleep otherwise 100% cpu even if no frame is asked for.
+            time.sleep(0.01)
 
 
 #