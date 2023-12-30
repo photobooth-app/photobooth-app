bc5c4677e7297d922ee76d356cc1ce9a46a8ad25
michael <me@mgrl.de>
2023-12-30 12:55:55 +0100

    improved test coverage

--- tests/test_backends_allplatforms.py
+++ tests/test_backends_allplatforms.py
@@ -57,7 +57,7 @@ def backend_virtual() -> VirtualCameraBackend:
     backend.stop()
 
 
-def test_get_images_virtualcamera(backend_virtual: WebcamCv2Backend):
+def test_get_images_virtualcamera(backend_virtual: VirtualCameraBackend):
     """get lores and hires images from backend and assert"""
     get_images(backend_virtual)
 
@@ -65,3 +65,26 @@ def test_get_images_virtualcamera(backend_virtual: WebcamCv2Backend):
 def test_get_images_webcamcv2(backend_cv2: WebcamCv2Backend):
     """get lores and hires images from backend and assert"""
     get_images(backend_cv2)
+
+
+def test_get_livestream_virtualcamera(backend_virtual: VirtualCameraBackend):
+    g_stream = backend_virtual.gen_stream()
+
+    i = 0
+    for frame in g_stream:  # frame is bytes
+        frame: bytes
+
+        # ensure we always receive a valid jpeg frame, nothing else.
+        # in case the backend failed, we receive a substitute image
+        assert frame.startswith(b"--frame\r\nContent-Type: image/jpeg\r\n\r\n\xff\xd8\xff")
+
+        if i == 5:
+            # we could add a way here to have virtualcamera failing to provide images (similar like for digicamcontrol)
+            pass
+
+        if i > 15:
+            g_stream.close()
+
+        i = i + 1
+
+    get_images(backend_virtual)
--- tests/test_backends_windows.py
+++ tests/test_backends_windows.py
@@ -1,8 +1,8 @@
 import logging
 import platform
-import time
 
 import pytest
+import requests
 
 from photobooth.services.backends.digicamcontrol import DigicamcontrolBackend
 from photobooth.services.config import appconfig
@@ -40,23 +40,6 @@ def backend_digicamcontrol() -> DigicamcontrolBackend:
 
     logger.info(f"available camera indexes: {_availableCameraIndexes}")
 
-    time.sleep(2)  # wait little time until backend is ready to deliver. otherwise HQ image would miss the event to trigger
-
-    # backends_container.config().backends.digicamcontrol_base_url = httpserver.url_for("/")
-    # logger.info(f"set mockup testserver: {backends_container.config().backends.digicamcontrol_base_url}")
-
-    # with open("tests/assets/input_lores.jpg", "rb") as file:
-    #     in_file_read = file.read()
-
-    # httpserver.expect_request("/", query_string="CMD=LiveViewWnd_Show", method="GET").respond_with_data("OK")
-    # httpserver.expect_request("/", query_string="CMD=All_Minimize", method="GET").respond_with_data("OK")
-    # httpserver.expect_request("/", query_string="slc=list&param1=cameras&param2=", method="GET").respond_with_data("camera1mockup")
-    # httpserver.expect_request("/", query_string="slc=set&param1=session.folder&param2=", method="GET").respond_with_data("OK")
-    # httpserver.expect_request("/", query_string="slc=set&param1=session.filenametemplate&param2=", method="GET").respond_with_data("OK")
-    # httpserver.expect_request("/", query_string="slc=capture&param1=&param2=", method="GET").respond_with_data("OK")
-    # httpserver.expect_request("/liveview.jpg", method="GET").respond_with_data(in_file_read, mimetype="image/jpeg")
-    # httpserver.expect_request("/", method="GET").respond_with_data("OK")
-
     # deliver
     backend.start()
     yield backend
@@ -65,5 +48,47 @@ def backend_digicamcontrol() -> DigicamcontrolBackend:
 
 def test_get_images_digicamcontrol(backend_digicamcontrol: DigicamcontrolBackend):
     # get lores and hires images from backend and assert
+
+    get_images(backend_digicamcontrol)
+
+
+def test_get_images_disable_liveview(backend_digicamcontrol: DigicamcontrolBackend):
+    # get one image to ensure it's working
+    get_images(backend_digicamcontrol)
+
+    # disable liveview to test exceptions and if it recovers properly
+    session = requests.Session()
+    r = session.get(f"{appconfig.backends.digicamcontrol_base_url}/?CMD=LiveViewWnd_Hide")
+    assert r.status_code == 200
+    if not r.ok:
+        raise AssertionError(f"error disabling liveview {r.status_code} {r.text}")
+
+    get_images(backend_digicamcontrol)
+
+
+def test_get_livestream(backend_digicamcontrol: DigicamcontrolBackend):
+    g_stream = backend_digicamcontrol.gen_stream()
+
+    i = 0
+    for frame in g_stream:  # frame is bytes
+        frame: bytes
+
+        # ensure we always receive a valid jpeg frame, nothing else.
+        # in case the backend failed, we receive a substitute image
+        assert frame.startswith(b"--frame\r\nContent-Type: image/jpeg\r\n\r\n\xff\xd8\xff")
+
+        if i == 5:
+            logger.info("disable liveview to test exceptions and if it recovers properly")
+
+            session = requests.Session()
+            r = session.get(f"{appconfig.backends.digicamcontrol_base_url}/?CMD=LiveViewWnd_Hide")
+            assert r.status_code == 200
+            if not r.ok:
+                raise AssertionError(f"error disabling liveview {r.status_code} {r.text}")
+
+        if i > 15:
+            g_stream.close()
+
+        i = i + 1
 
     get_images(backend_digicamcontrol)
