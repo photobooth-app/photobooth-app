356b8547c7da62a5b46734cef00fea1acddff0be
michael <me@mgrl.de>
2024-01-28 17:37:41 +0100

    remove different hires-lores data from cv2 backend since not really useful

--- photobooth/services/backends/webcamcv2.py
+++ photobooth/services/backends/webcamcv2.py
@@ -27,7 +27,7 @@ class WebcamCv2Backend(AbstractBackend):
         super().__init__()
         self._failing_wait_for_lores_image_is_error = True  # missing lores images is automatically considered as error
 
-        self._img_buffer_lores = SharedMemoryDataExch(
+        self._img_buffer = SharedMemoryDataExch(
             sharedmemory=shared_memory.SharedMemory(
                 create=True,
                 size=SHARED_MEMORY_BUFFER_BYTES,
@@ -36,28 +36,15 @@ class WebcamCv2Backend(AbstractBackend):
             lock=Lock(),
         )
 
-        self._img_buffer_hires = SharedMemoryDataExch(
-            sharedmemory=shared_memory.SharedMemory(
-                create=True,
-                size=SHARED_MEMORY_BUFFER_BYTES,
-            ),
-            condition=Condition(),
-            lock=Lock(),
-        )
-
-        self._event_hq_capture: Event = Event()
         self._event_proc_shutdown: Event = Event()
 
         self._cv2_process: Process = None
 
     def __del__(self):
         try:
-            if self._img_buffer_lores:
-                self._img_buffer_lores.sharedmemory.close()
-                self._img_buffer_lores.sharedmemory.unlink()
-            if self._img_buffer_hires:
-                self._img_buffer_hires.sharedmemory.close()
-                self._img_buffer_hires.sharedmemory.unlink()
+            if self._img_buffer:
+                self._img_buffer.sharedmemory.close()
+                self._img_buffer.sharedmemory.unlink()
         except Exception as exc:
             # cant use logger any more, just to have some logs to debug if exception
             print(exc)
@@ -72,13 +59,9 @@ class WebcamCv2Backend(AbstractBackend):
             target=cv2_img_aquisition,
             name="WebcamCv2AquisitionProcess",
             args=(
-                self._img_buffer_lores.sharedmemory.name,
-                self._img_buffer_hires.sharedmemory.name,
-                self._img_buffer_lores.lock,
-                self._img_buffer_hires.lock,
-                self._event_hq_capture,
-                self._img_buffer_lores.condition,
-                self._img_buffer_hires.condition,
+                self._img_buffer.sharedmemory.name,
+                self._img_buffer.lock,
+                self._img_buffer.condition,
                 appconfig,
                 self._event_proc_shutdown,
             ),
@@ -117,18 +100,7 @@ class WebcamCv2Backend(AbstractBackend):
 
     def _wait_for_hq_image(self):
         """for other threads to receive a hq JPEG image"""
-
-        # get img off the producing queue
-        with self._img_buffer_hires.condition:
-            self._event_hq_capture.set()
-
-            if not self._img_buffer_hires.condition.wait(timeout=4):
-                raise TimeoutError("timeout receiving frames")
-
-            with self._img_buffer_hires.lock:
-                img = decompile_buffer(self._img_buffer_hires.sharedmemory)
-
-        return img
+        return self._wait_for_lores_image()
 
     #
     # INTERNAL FUNCTIONS
@@ -137,12 +109,12 @@ class WebcamCv2Backend(AbstractBackend):
     def _wait_for_lores_image(self):
         """for other threads to receive a lores JPEG image"""
 
-        with self._img_buffer_lores.condition:
-            if not self._img_buffer_lores.condition.wait(timeout=0.2):
+        with self._img_buffer.condition:
+            if not self._img_buffer.condition.wait(timeout=0.2):
                 raise TimeoutError("timeout receiving frames")
 
-            with self._img_buffer_lores.lock:
-                img = decompile_buffer(self._img_buffer_lores.sharedmemory)
+            with self._img_buffer.lock:
+                img = decompile_buffer(self._img_buffer.sharedmemory)
             return img
 
     def _on_configure_optimized_for_hq_capture(self):
@@ -158,13 +130,9 @@ class WebcamCv2Backend(AbstractBackend):
 
 
 def cv2_img_aquisition(
-    shm_buffer_lores_name,
-    shm_buffer_hires_name,
-    _img_buffer_lores_lock,
-    _img_buffer_hires_lock,
-    _event_hq_capture: Event,
-    _condition_img_buffer_lores_ready: Condition,
-    _condition_img_buffer_hires_ready: Condition,
+    shm_buffer_name,
+    _img_buffer_lock,
+    _condition_img_buffer_ready: Condition,
     # need to pass config, because unittests can change config,
     # if not passed, the config are not available in the separate process!
     _config: AppConfig,
@@ -180,8 +148,7 @@ def cv2_img_aquisition(
     fmt = "%(asctime)s [%(levelname)8s] %(message)s (%(filename)s:%(lineno)s) proc%(process)d"
     logging.basicConfig(level=logging.DEBUG, format=fmt)
 
-    shm_lores = shared_memory.SharedMemory(shm_buffer_lores_name)
-    shm_hires = shared_memory.SharedMemory(shm_buffer_hires_name)
+    shm = shared_memory.SharedMemory(shm_buffer_name)
 
     if platform.system() == "Windows":
         logger.info("force VideoCapture to DSHOW backend on windows (MSMF is buggy and crashes app)")
@@ -220,34 +187,15 @@ def cv2_img_aquisition(
         if _config.backends.cv2_CAMERA_TRANSFORM_VFLIP:
             array = cv2.flip(array, 0)
 
-        if _event_hq_capture.is_set():
-            _event_hq_capture.clear()
+        # preview livestream
+        jpeg_buffer = turbojpeg.encode(array, quality=90)
+        # put jpeg on queue until full. If full this function blocks until queue empty
+        with _img_buffer_lock:
+            compile_buffer(shm, jpeg_buffer)
 
-            # one time hq still
-
-            # array = cv2.fastNlMeansDenoisingColored(array, None, 2, 2, 3, 9)
-            # above command takes too long time -> timeout on wait commands
-            # HD frame needs like 2sec, not suitable for realtime processing
-
-            # convert frame to jpeg buffer
-            jpeg_buffer = turbojpeg.encode(array, quality=_config.mediaprocessing.HIRES_STILL_QUALITY)
-            # put jpeg on queue until full. If full this function blocks until queue empty
-            with _img_buffer_hires_lock:
-                compile_buffer(shm_hires, jpeg_buffer)
-
-            with _condition_img_buffer_hires_ready:
-                # wait to be notified
-                _condition_img_buffer_hires_ready.notify_all()
-        else:
-            # preview livestream
-            jpeg_buffer = turbojpeg.encode(array, quality=_config.mediaprocessing.LIVEPREVIEW_QUALITY)
-            # put jpeg on queue until full. If full this function blocks until queue empty
-            with _img_buffer_lores_lock:
-                compile_buffer(shm_lores, jpeg_buffer)
-
-            with _condition_img_buffer_lores_ready:
-                # wait to be notified
-                _condition_img_buffer_lores_ready.notify_all()
+        with _condition_img_buffer_ready:
+            # wait to be notified
+            _condition_img_buffer_ready.notify_all()
 
     # release camera on process shutdown
     _video.release()
