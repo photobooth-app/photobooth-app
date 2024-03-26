1370580a0373ab3517b302e2afb2ed77eb708e0b
Michael <michael@groene.de>
2024-03-26 16:19:03 +0900

    update frontend

--- photobooth/web_spa/assets/QSpace-BFpe6e4K.js
+++
@@ -1,1 +0,0 @@
-import{t as e,v as a}from"./index-DVgiKa8q.js";const s=e("div",{class:"q-space"}),c=a({name:"QSpace",setup(){return()=>s}});export{c as Q};
--- photobooth/web_spa/assets/use-quasar-DgAoFPc7.js
+++
@@ -1,1 +0,0 @@
-import{a4 as a,aC as r}from"./index-DVgiKa8q.js";function u(){return a(r)}export{u};
