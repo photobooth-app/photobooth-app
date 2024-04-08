7794eda055e5f63d466ddb095c007d4cfeebfa84
michael <me@mgrl.de>
2024-04-08 21:33:07 +0200

    improve frontend countdown ux

--- photobooth/web_spa/assets/use-quasar-BwAzHUFh.js
+++
@@ -1,1 +0,0 @@
-import{a4 as a,aC as r}from"./index-D48yhi-H.js";function u(){return a(r)}export{u};
---
+++ photobooth/web_spa/assets/use-quasar-CzF_ntFF.js
@@ -0,0 +1,1 @@
+import{a4 as a,aC as r}from"./index-BtrfCjc9.js";function u(){return a(r)}export{u};
