7c9bd2e5d33d1279746cdc3f6822b5a78a190da6
michael <me@mgrl.de>
2024-04-08 18:28:58 +0200

    update frontend

---
+++ photobooth/web_spa/assets/use-quasar-DjLdC7SX.js
@@ -0,0 +1,1 @@
+import{a4 as a,aC as r}from"./index-DcsdTz6E.js";function u(){return a(r)}export{u};
--- photobooth/web_spa/assets/use-quasar-k0CX-VYi.js
+++
@@ -1,1 +0,0 @@
-import{a4 as a,aC as r}from"./index-B0JvbMKz.js";function u(){return a(r)}export{u};
