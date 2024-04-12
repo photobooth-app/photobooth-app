f34967c12c656bcc7e9d4606e0d4b26f7571c5e8
michael <me@mgrl.de>
2024-04-12 20:38:55 +0200

    update frontend

--- photobooth/web_spa/assets/QSpace-CSj92DpM.js
+++
@@ -1,1 +0,0 @@
-import{t as e,v as a}from"./index-BHS93q0p.js";const s=e("div",{class:"q-space"}),c=a({name:"QSpace",setup(){return()=>s}});export{c as Q};
--- photobooth/web_spa/assets/use-quasar-CUHfjdeJ.js
+++
@@ -1,1 +0,0 @@
-import{a4 as a,aC as r}from"./index-BHS93q0p.js";function u(){return a(r)}export{u};
