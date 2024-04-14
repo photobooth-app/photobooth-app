5c3ec2084b5569f16c18460347ba6c999588bf4c
michael <me@mgrl.de>
2024-04-14 18:08:11 +0200

    test gphoto2 backend util

--- photobooth/routers/api_admin/__init__.py
+++ photobooth/routers/api_admin/__init__.py
@@ -2,13 +2,15 @@
 
 from fastapi import APIRouter
 
-from . import config, files
+from . import config, files, utils
 
 __all__ = [
     "config",  # refers to the 'config.py' file
     "files",
+    "utils",
 ]
 
 router = APIRouter(prefix="/api/admin")
 router.include_router(config.router)
 router.include_router(files.router)
+router.include_router(utils.router)
