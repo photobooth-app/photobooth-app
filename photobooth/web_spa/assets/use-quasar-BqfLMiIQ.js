f23cb95c8e1d48020e044f8dcef377e0938f1953
michael <me@mgrl.de>
2024-04-23 20:52:33 +0200

    improve darkmode

---
+++ photobooth/web_spa/assets/use-quasar-BqfLMiIQ.js
@@ -0,0 +1,1 @@
+import{a6 as a,az as r}from"./index-agTN-YSu.js";function u(){return a(r)}export{u};
--- photobooth/web_spa/assets/use-quasar-sn6yD2dM.js
+++
@@ -1,1 +0,0 @@
-import{a6 as a,az as r}from"./index-CNzCL5vR.js";function u(){return a(r)}export{u};
