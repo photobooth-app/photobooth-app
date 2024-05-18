62ac38e333d899b46b4d36557f4f1f2edfc450c4
michael <me@mgrl.de>
2024-05-18 16:38:48 +0200

    update frontend fix printer api endpoint

---
+++ photobooth/web_spa/assets/use-quasar-BDsy8r4j.js
@@ -0,0 +1,1 @@
+import{ac as a,aE as r}from"./index-D3nrDivs.js";function u(){return a(r)}export{u};
--- photobooth/web_spa/assets/use-quasar-BecjQ80m.js
+++
@@ -1,1 +0,0 @@
-import{ac as a,aE as r}from"./index-CRM5C-vp.js";function u(){return a(r)}export{u};
