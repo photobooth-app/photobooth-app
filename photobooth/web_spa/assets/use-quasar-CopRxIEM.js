1674d208cd83b07d7c614550b0a7e1bd418ba2c0
michael <me@mgrl.de>
2024-05-26 19:54:59 +0200

    update frontend to implement #273

--- photobooth/web_spa/assets/use-quasar-CopRxIEM.js
+++
@@ -1,1 +0,0 @@
-import{an as a,aL as r}from"./index-PtNPAqMO.js";function u(){return a(r)}export{u};
---
+++ photobooth/web_spa/assets/use-quasar-DrxZ3kTl.js
@@ -0,0 +1,1 @@
+import{an as a,aL as r}from"./index-y2DIdkHj.js";function u(){return a(r)}export{u};
