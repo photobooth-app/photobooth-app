d2542bf4c3815363497cf3bc77a91b8e5e2afe0d
michael <me@mgineer85.de>
2024-06-23 20:35:15 +0200

    create basic folders also during tests before these run.

--- tests/tests/conftest.py
+++ tests/tests/conftest.py
@@ -2,14 +2,21 @@ import logging
 
 import pytest
 
+from photobooth.__main__ import _create_basic_folders
 from photobooth.container import container
 from photobooth.services.config import appconfig
 
 logger = logging.getLogger(name=None)
 
 
-@pytest.fixture(scope="function", autouse=True)
+@pytest.fixture(scope="session", autouse=True)
 def global_session_setup():
+    logger.debug("create basic folders")
+    _create_basic_folders()
+
+
+@pytest.fixture(scope="function", autouse=True)
+def global_function_setup1():
     logger.info("global function-scoped mediaitem setup")
 
     if container.mediacollection_service.number_of_images == 0:
@@ -24,7 +31,7 @@ def global_session_setup():
 
 
 @pytest.fixture(scope="function", autouse=True)
-def global_function_setup():
+def global_function_setup2():
     logger.info("global function-scoped appconfig reset and optimization for speed reasons")
 
     appconfig.reset_defaults()
