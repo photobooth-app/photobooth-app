ae79b62061096759995a31099576cd3cfd905e95
michael <me@mgineer85.de>
2024-07-17 07:32:57 +0200

    update frontend

---
+++ photobooth/web_spa/assets/use-quasar-Bi2BZxyv.js
@@ -0,0 +1,1 @@
+import{aj as a,aI as r}from"./index-BcYb4QxB.js";function u(){return a(r)}export{u};
--- photobooth/web_spa/assets/use-quasar-C5okrZKB.js
+++
@@ -1,1 +0,0 @@
-import{aj as a,aI as r}from"./index-BMVz2vNn.js";function u(){return a(r)}export{u};
