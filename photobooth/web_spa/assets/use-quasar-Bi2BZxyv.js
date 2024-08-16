3a95d58bd604826c03a2b356c507a5f50add6250
michael <me@mgineer85.de>
2024-08-16 07:58:49 +0200

    update frontend (updated deps and improved random slideshow)

--- photobooth/web_spa/assets/use-quasar-Bi2BZxyv.js
+++
@@ -1,1 +0,0 @@
-import{aj as a,aI as r}from"./index-BcYb4QxB.js";function u(){return a(r)}export{u};
---
+++ photobooth/web_spa/assets/use-quasar-D6tCIMc4.js
@@ -0,0 +1,1 @@
+import{aj as a,aI as r}from"./index-Chxxixq4.js";function u(){return a(r)}export{u};
