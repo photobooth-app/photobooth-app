c75dd1efa00c8c18d4f6dc61cdbdb18ca667c5bb
michael <me@mgineer85.de>
2024-11-23 10:09:23 +0100

    fix wigglecam integration test.

--- tests/tests/test_backends_allplatforms_wigglecam.py
+++ tests/tests/test_backends_allplatforms_wigglecam.py
@@ -1,15 +1,16 @@
-import io
 import logging
-import time
 from collections.abc import Generator
 from multiprocessing import Process
 
 import pytest
 import uvicorn
-from PIL import Image
 from wigglecam import __main__
-from wigglecam.connector import CameraNode
-from wigglecam.connector.models import ConfigCameraNode
+from wigglecam.container import Container
+
+from photobooth.services.backends.wigglecam import WigglecamBackend
+from photobooth.services.config.groups.backends import ConfigCameraNode, GroupBackendWigglecam
+
+from .utils import get_images
 
 logger = logging.getLogger(name=None)
 
@@ -19,22 +20,26 @@ def run_server():
 
 
 @pytest.fixture(scope="module")
-def emulated_node() -> Generator[CameraNode, None, None]:
+def emulated_node() -> Generator[Container, None, None]:
     proc = Process(target=run_server, args=(), daemon=True)
     proc.start()
-
-    node = CameraNode(ConfigCameraNode(description="test", base_url="http://127.0.0.1:8081"))
-    while not node.can_connect:
-        logger.debug("waiting until emulated server up and running")
-        time.sleep(0.4)
-
-    yield node
+    yield __main__.container
     proc.kill()  # Cleanup after test
 
 
-def test_read_still(emulated_node):
-    try:
-        with Image.open(io.BytesIO(emulated_node.camera_still())) as img:
-            img.verify()
-    except Exception as exc:
-        raise AssertionError(f"backend did not return valid image bytes, {exc}") from exc
+@pytest.fixture()
+def backend_wigglecam() -> Generator[WigglecamBackend, None, None]:
+    backend = WigglecamBackend(
+        GroupBackendWigglecam(
+            nodes=[ConfigCameraNode(description="test", base_url="http://127.0.0.1:8081")],
+        )
+    )
+
+    backend.start()
+    backend.block_until_device_is_running()
+    yield backend
+    backend.stop()
+
+
+def test_read_still(emulated_node, backend_wigglecam):
+    get_images(backend_wigglecam)
