4eb0c9c9f4f936e59c187165ea44e1531e3caaee
michael <me@mgineer85.de>
2024-12-13 21:29:01 +0100

    add pyav for videoprocessing. also includes ffmpeg as binary on win and linux.

--- tests/benchmarks/benchmark_create_video_test.py
+++ tests/benchmarks/benchmark_create_video_test.py
@@ -16,79 +16,8 @@ def run_around_tests():
     yield
 
 
-# def process_ffmpeg_binding(tmp_path):
-#     ffmpeg = (
-#         FFmpeg()
-#         .option("y")
-#         .input("tests/tests/WebcamCv2Backend_881d87a13424452ca1bfe9ee65ca130b.mjpeg")
-#         .output(
-#             str(tmp_path / "ffmpeg_scale.mp4"),
-#             {
-#                 "codec:v": "libx264",
-#             },
-#             preset="veryfast",
-#             crf=24,
-#         )
-#     )
-
-#     @ffmpeg.on("start")
-#     def on_start(arguments: list[str]):
-#         print("arguments:", arguments)
-
-#     @ffmpeg.on("progress")
-#     def on_progress(progress: Progress):
-#         print(progress)
-
-#     tms = time.time()
-#     ffmpeg.execute()
-#     logger.debug(f"-- process time: {round((time.time() - tms), 2)}s ")
-#     raise AssertionError
-
-
-# # basic idea from https://stackoverflow.com/a/42602576
-# def process_ffmpeg_mjpeg(tmp_path):
-#     # return
-#     logger.info("popen")
-#     tms = time.time()
-#     ffmpeg_subprocess = Popen(
-#         [
-#             "ffmpeg",
-#             "-y",  # overwrite with no questions
-#             "-loglevel",
-#             "warning",
-#             # "-vcodec",
-#             # "mjpeg",
-#             # "-framerate",
-#             # "30",
-#             "-i",
-#             "tests/tests/WebcamCv2Backend_881d87a13424452ca1bfe9ee65ca130b.mjpeg",
-#             # "-tune",
-#             # "zerolatency",
-#             "-vcodec",
-#             "libx264",  # warning! image height must be divisible by 2! #there are also hw encoder avail: https://stackoverflow.com/questions/50693934/different-h264-encoders-in-ffmpeg
-#             "-preset",
-#             "veryfast",
-#             "-b:v",  # bitrate
-#             "5000k",
-#             "-movflags",
-#             "+faststart",
-#             str(tmp_path / "ffmpeg_scale.mp4"),
-#         ]
-#     )
-#     logger.info("popen'ed")
-#     logger.debug(f"-- process time: {round((time.time() - tms), 2)}s ")
-
-#     # now postproccess, but app can continue.
-#     tms = time.time()
-#     code = ffmpeg_subprocess.wait()
-#     logger.info("finished waiting.")
-#     logger.debug(f"-- process time: {round((time.time() - tms), 2)}s ")
-#     if code != 0:
-#         # more debug info can be received in ffmpeg popen stderr (pytest captures automatically)
-#         # TODO: check how to get in application at runtime to write to logs or maybe let ffmpeg write separate logfile
-#         raise RuntimeError(f"error creating videofile, ffmpeg exit code ({code}).")
-
-#     raise AssertionError
+def process_pyav(tmp_path):
+    pass
 
 
 # basic idea from https://stackoverflow.com/a/42602576
@@ -154,3 +83,8 @@ def process_ffmpeg(tmp_path):
 @pytest.mark.benchmark(group="create_video_from_pipe_to_h264")
 def test_process_ffmpeg(benchmark, tmp_path):
     benchmark(process_ffmpeg, tmp_path=tmp_path)
+
+
+@pytest.mark.benchmark(group="create_video_from_pipe_to_h264")
+def test_process_pyav(benchmark, tmp_path):
+    benchmark(process_pyav, tmp_path=tmp_path)
