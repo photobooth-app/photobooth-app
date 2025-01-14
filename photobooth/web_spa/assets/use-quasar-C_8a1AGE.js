7a32579f29654ee96a795cae7da2d0f7e9aeeb22
michael <me@mgineer85.de>
2025-01-14 20:30:27 +0100

    update frontend

---
+++ photobooth/web_spa/assets/use-quasar-C_8a1AGE.js
@@ -0,0 +1,1 @@
+import{ai as a,aG as r}from"./index-DNM8q3_g.js";function u(){return a(r)}export{u};
--- photobooth/web_spa/assets/use-quasar-DtmQNbi3.js
+++
@@ -1,1 +0,0 @@
-import{ai as a,aG as r}from"./index-BNUf-CLs.js";function u(){return a(r)}export{u};
