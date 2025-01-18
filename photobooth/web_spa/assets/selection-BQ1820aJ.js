d75e117f356309d58a5ba48adad34559f0267620
michael <me@mgineer85.de>
2025-01-18 21:32:07 +0100

    update frontend

--- photobooth/web_spa/assets/selection-BQ1820aJ.js
+++
@@ -1,1 +0,0 @@
-import{X as n}from"./index-Bu4TEJL3.js";function i(){if(window.getSelection!==void 0){const e=window.getSelection();e.empty!==void 0?e.empty():e.removeAllRanges!==void 0&&(e.removeAllRanges(),n.is.mobile!==!0&&e.addRange(document.createRange()))}else document.selection!==void 0&&document.selection.empty()}export{i as c};
