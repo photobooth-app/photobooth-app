7df13c50841ce78708ec368790c11ac2ef2c817a
michael <me@mgineer85.de>
2025-01-18 18:00:22 +0100

    improve end2end benchmarks.

--- tests/benchmarks/conftest.py
+++ tests/benchmarks/conftest.py
@@ -15,6 +15,14 @@ def global_function_setup2():
     appconfig.reset_defaults()
 
     appconfig.actions.image[0].jobcontrol.countdown_capture = 0.0
+    appconfig.actions.collage[0].jobcontrol.countdown_capture = 0.0
+    appconfig.actions.collage[0].jobcontrol.countdown_capture_second_following = 0.0
+    appconfig.actions.collage[0].jobcontrol.approve_autoconfirm_timeout = 0.0
+    appconfig.actions.animation[0].jobcontrol.countdown_capture = 0.0
+    appconfig.actions.animation[0].jobcontrol.countdown_capture_second_following = 0.0
+    appconfig.actions.animation[0].jobcontrol.approve_autoconfirm_timeout = 0.0
+    appconfig.actions.video[0].jobcontrol.countdown_capture = 0.0
+    appconfig.actions.multicamera[0].jobcontrol.countdown_capture = 0.0
 
     yield
 
--- tests/benchmarks/end_to_end_capture_test.py
+++ tests/benchmarks/end_to_end_capture_test.py
@@ -1,12 +1,8 @@
 import logging
 
 import pytest
-from sqlalchemy import select, text
-from sqlalchemy.orm import Session
 
 from photobooth.container import Container, container
-from photobooth.database.database import engine
-from photobooth.database.models import Mediaitem
 from photobooth.database.types import DimensionTypes
 from photobooth.routers.media import api_getitems
 
@@ -22,28 +18,41 @@ def _container() -> Container:
     container.stop()
 
 
-def wait_for_still(_container: Container):
-    # this test is probably just measuring how fast the backend delivers, not the processing itself.
-    _container.aquisition_service.wait_for_still_file()
-
-
-def end_to_end_still(_container: Container):
-    container.processing_service.trigger_action("image", 0)
+def do_end_to_end(_container: Container, action: str):
+    # 1: capture an image
+    _container.processing_service.trigger_action(action, 0)
     _container.processing_service.wait_until_job_finished()
 
-    # get last element by last_insert_rowid instead collections getlastitem because datetime resolution is only 1 sec
-    with Session(engine) as session:
-        res = session.scalar(text("SELECT last_insert_rowid() FROM mediaitems"))
-        last_id = session.scalars(select(Mediaitem.id).where(text(f"rowid=={res}"))).one()
+    # -: get latest item. needed for test only, but should have only little effect on benchmark
+    item = _container.mediacollection_service.get_item_latest()
 
-    api_getitems(last_id, DimensionTypes.preview)
+    # 2: emulate preview generation that can be downloaded by the client then
+    api_getitems(item.id, DimensionTypes.preview)
 
-
-@pytest.mark.benchmark()
-def test_wait_for_still(benchmark, _container: Container):
-    benchmark(wait_for_still, _container)
+    # job done.
 
 
-@pytest.mark.benchmark()
-def test_end_to_end_still(benchmark, _container: Container):
-    benchmark(end_to_end_still, _container)
+@pytest.mark.benchmark(group="end_to_end_actions")
+def test_end_to_end_image(benchmark, _container: Container):
+    benchmark(do_end_to_end, _container, "image")
+
+
+@pytest.mark.benchmark(group="end_to_end_actions")
+def test_end_to_end_collage(benchmark, _container: Container):
+    benchmark(do_end_to_end, _container, "collage")
+
+
+@pytest.mark.benchmark(group="end_to_end_actions")
+def test_end_to_end_video(benchmark, _container: Container):
+    # TODO: video recording would need to be subtracted from results. ...
+    benchmark(do_end_to_end, _container, "video")
+
+
+@pytest.mark.benchmark(group="end_to_end_actions")
+def test_end_to_end_animation(benchmark, _container: Container):
+    benchmark(do_end_to_end, _container, "animation")
+
+
+@pytest.mark.benchmark(group="end_to_end_actions")
+def test_end_to_end_multicamera(benchmark, _container: Container):
+    benchmark(do_end_to_end, _container, "multicamera")
