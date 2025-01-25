0daf66af14d4cb9de40984e1f6ceb9407a545c4e
michael <me@mgineer85.de>
2025-01-25 22:12:55 +0100

    update frontend

---
+++ src/photobooth/web_spa/assets/selection-EFw24aFD.js
@@ -0,0 +1,1 @@
+import{U as n}from"./index-BNYqvihj.js";function i(){if(window.getSelection!==void 0){const e=window.getSelection();e.empty!==void 0?e.empty():e.removeAllRanges!==void 0&&(e.removeAllRanges(),n.is.mobile!==!0&&e.addRange(document.createRange()))}else document.selection!==void 0&&document.selection.empty()}export{i as c};
