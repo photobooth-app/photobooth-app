a9672e33c033e2dc4127200addbc4e1c2bc01e26
michael <me@mgineer85.de>
2025-01-26 13:19:57 +0100

    sanitize userinput to fix security scanning issues.

--- src/photobooth/routers/userdata.py
+++ src/photobooth/routers/userdata.py
@@ -1,11 +1,10 @@
 import logging
-from pathlib import Path
 
 from fastapi import APIRouter, HTTPException
 from fastapi.responses import FileResponse
 
 from .. import USERDATA_PATH
-from ..utils.helper import get_user_file
+from ..utils.helper import filenames_sanitize, get_user_file
 
 logger = logging.getLogger(__name__)
 userdata_router = APIRouter(
@@ -15,9 +14,10 @@ userdata_router = APIRouter(
 
 
 @userdata_router.get("/{filepath:path}")
-def api_get_userfiles(filepath: Path):
+def api_get_userfiles(filepath: str):
     try:
-        return FileResponse(path=get_user_file(Path(USERDATA_PATH) / filepath))
+        filepath_sanitized = filenames_sanitize(f"{USERDATA_PATH}{filepath}")
+        return FileResponse(path=get_user_file(filepath_sanitized))
     except FileNotFoundError as exc:
         logger.warning(f"cannot find {filepath}")
         raise HTTPException(status_code=404, detail=f"cannot find {filepath}") from exc
