965d7909f673b36e6b8eabc9aa4ffad13753532e
michael <me@mgineer85.de>
2025-02-08 10:00:31 +0100

    update tests for latest config

--- src/tests/tests/router/api/test_config.py
+++ src/tests/tests/router/api/test_config.py
@@ -25,21 +25,17 @@ def client_authenticated(client) -> TestClient:
     yield client
 
 
-@pytest.fixture(
-    params=[
-        "/config/current",  # without password protection, UI reads the config
-        "/admin/config/schema?schema_type=dereferenced",
-        "/admin/config/current",
-    ]
-)
-def config_endpoint(request):
-    # setup
-    yield request.param
-    # cleanup
+def test_config_endpoints_ui(client: TestClient):
+    response = client.get("/config")
+    assert response.status_code == 200
+
+    AppConfig.model_validate(response.json())
 
 
-def test_config_endpoints(client_authenticated: TestClient, config_endpoint):
-    response = client_authenticated.get(config_endpoint)
+def test_config_getschema(client_authenticated: TestClient):
+    response = client_authenticated.get("/admin/config/app/schema")
+    assert response.status_code == 200
+    response = client_authenticated.get("/admin/config/app/schema?schema_type=dereferenced")
     assert response.status_code == 200
 
 
@@ -48,7 +44,7 @@ def test_config_post_validationerror(client_authenticated: TestClient):
     # set illegal setting, that provokes validation error
     config_dict["common"]["logging_level"] = "illegalsetting"
 
-    response = client_authenticated.post("/admin/config/current", json=config_dict)
+    response = client_authenticated.patch("/admin/config/app", json=config_dict)
 
     assert response.status_code == 422
 
@@ -56,11 +52,11 @@ def test_config_post_validationerror(client_authenticated: TestClient):
     AppConfig().deleteconfig()
 
 
-def test_config_post(client_authenticated: TestClient):
+def test_config_patch(client_authenticated: TestClient):
     # jsonify using pydantic's json function, because fastapi cannot convert all types (like Color)
     config_dict = json.loads(AppConfig().model_dump_json(context={"secrets_is_allowed": True}))
 
-    response = client_authenticated.post("/admin/config/current", json=config_dict)
+    response = client_authenticated.patch("/admin/config/app", json=config_dict)
     assert response.status_code == 200
 
     # config is changed by this command - revert it do avoid affecting other tests.
@@ -69,7 +65,7 @@ def test_config_post(client_authenticated: TestClient):
 
 @patch("os.remove")
 def test_config_reset(mock_remove, client_authenticated: TestClient):
-    response = client_authenticated.get("/admin/config/reset")
+    response = client_authenticated.delete("/admin/config/app")
 
     assert response.status_code == 200
 
