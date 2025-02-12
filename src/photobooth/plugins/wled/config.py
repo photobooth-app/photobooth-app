3b49e17781aebc9cabc3b38265c941cbe7a6cce4
michael <me@mgineer85.de>
2025-02-12 20:03:48 +0100

    update env vars for wled plugin

--- src/photobooth/plugins/wled/config.py
+++ src/photobooth/plugins/wled/config.py
@@ -6,7 +6,11 @@ from photobooth.services.config.baseconfig import BaseConfig
 
 
 class WledConfig(BaseConfig):
-    model_config = SettingsConfigDict(title="WLED Plugin Config", json_file=f"{CONFIG_PATH}plugin_wled.json")
+    model_config = SettingsConfigDict(
+        title="WLED Plugin Config",
+        json_file=f"{CONFIG_PATH}plugin_wled.json",
+        env_prefix="wled-",
+    )
 
     wled_enabled: bool = Field(
         default=False,
