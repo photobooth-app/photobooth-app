ec23d40f6684d98396052630679172c78d00d051
michael <me@mgineer85.de>
2025-02-14 23:32:55 +0100

    allow browser to use cache after revalidation.

--- src/photobooth/routers/media.py
+++ src/photobooth/routers/media.py
@@ -1,8 +1,10 @@
 import logging
+import os
 from uuid import UUID
 
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Request, status
 from fastapi.responses import FileResponse
+from starlette.staticfiles import NotModifiedResponse, StaticFiles
 
 from ..container import container
 from ..database.models import DimensionTypes
@@ -15,15 +17,19 @@ media_router = APIRouter(
 
 
 @media_router.get("/{dimension}/{mediaitem_id}")
-def api_getitems(mediaitem_id: UUID, dimension: DimensionTypes):
+def api_getitems(mediaitem_id: UUID, dimension: DimensionTypes, request: Request):
     try:
-        # headers = {"Cache-Control": "max-age=86400, must-revalidate"}
-        headers = None
+        headers = {"Cache-Control": "no-cache"}
 
         item = container.mediacollection_service.get_item(mediaitem_id)
         cacheditem = container.mediacollection_service.cache.get_cached_repr(item, dimension, processed=True)
 
-        return FileResponse(path=cacheditem.filepath, headers=headers)
+        # StaticFiles has the check for modification on file basis, we borrow that for now to evaluate if 304 or 200 should be sent.
+        # TODO: later we can change the etag calc to something that is avail from the db anyways.
+        response = FileResponse(cacheditem.filepath, status_code=status.HTTP_200_OK, headers=headers, stat_result=os.stat(cacheditem.filepath))
+        if StaticFiles.is_not_modified(None, response_headers=response.headers, request_headers=request.headers):  # type: ignore
+            return NotModifiedResponse(response.headers)
+        return response
     except FileNotFoundError as exc:
         logger.warning(f"cannot find mediaitem by id {mediaitem_id}")
         raise HTTPException(status_code=404, detail=f"cannot find mediaitem by id {mediaitem_id}") from exc
