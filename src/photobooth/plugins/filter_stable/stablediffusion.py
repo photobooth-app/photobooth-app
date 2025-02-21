a8b4f7fc0f5f8a4eb5eb873117b9548bd30c69d2
soerennb <soeren@fledge.de>
2025-02-21 10:20:52 +0100

    Added getimg.ai and runware.ai providers

--- src/photobooth/plugins/filter_stable/stablediffusion.py
+++ src/photobooth/plugins/filter_stable/stablediffusion.py
@@ -30,7 +30,7 @@ class StableDiffusionFilter:
     def __init__(self, filter: str) -> None:
         self.filter = filter
 
-    def __call__(self, filter: str, image: Image.Image) -> Image.Image:
+    def __call__(self, filter: str, image: Image.Image ) -> Image.Image:
         try:
             baseparams = {
                 key: value
@@ -61,23 +61,23 @@ class StableDiffusionFilter:
                 module=params["openpose"]["module"],
                 model=params["openpose"]["model"],
                 weight=params["openpose"]["weight"],
-                threshold_a=0.5,
-                threshold_b=0.5,
-                resize_mode="Crop and Resize",
+                threshold_a = 0.5,
+                threshold_b = 0.5,
+                resize_mode="Crop and Resize"
             )
-            # openpose.input_mode = "simple"
-            # openpose.save_detected_map=True
-            # openpose.use_preview_as_input= False,
-            params.pop("openpose", None)
+            #openpose.input_mode = "simple"
+            #openpose.save_detected_map=True
+            #openpose.use_preview_as_input= False,
+            params.pop('openpose', None)
             controlnets.append(openpose)
 
             depth = ControlNetUnit(
-                module=params["depth"]["module"],
-                model=params["depth"]["model"],
+                module=params["depth"]["module"], 
+                model=params["depth"]["model"], 
                 weight=params["depth"]["weight"],
-                threshold_a=0.5,
-                threshold_b=0.5,
-                resize_mode="Crop and Resize",
+                threshold_a = 0.5,
+                threshold_b = 0.5,
+                resize_mode="Crop and Resize"
             )
             controlnets.append(depth)
             params.pop("depth", None)
@@ -86,9 +86,9 @@ class StableDiffusionFilter:
                 module=params["softedge"]["module"],
                 model=params["softedge"]["model"],
                 weight=params["softedge"]["weight"],
-                threshold_a=0.5,
-                threshold_b=0.5,
-                resize_mode="Crop and Resize",
+                threshold_a = 0.5,
+                threshold_b = 0.5,
+                resize_mode="Crop and Resize"
             )
             controlnets.append(softedge)
 
@@ -106,9 +106,9 @@ class StableDiffusionFilter:
             params["denoising_strength"] = float(params["denoising_strength"][0])
             params["cfg_scale"] = float(params["cfg_scale"][0])
             result = api.img2img(**params)
-            # for x in params["images"]:
+            #for x in params["images"]:
             #    print( b64_img(x))
-            # print( repr(params) )
+            #print( repr(params) )
             return result.image
 
             # optionally set username, password when --api-auth=username:password is set on webui.
