52be65b1270e0506af1308f860d22207a225b588
michael <me@mgineer85.de>
2025-04-06 12:44:17 +0200

    hide wigglecam action since it's not ready yet.

--- src/photobooth/services/config/groups/actions.py
+++ src/photobooth/services/config/groups/actions.py
@@ -476,7 +476,7 @@ class GroupActions(BaseModel):
                 jobcontrol=MulticameraJobControl(),
                 processing=MulticameraProcessing(),
                 trigger=Trigger(
-                    ui_trigger=UiTrigger(title="Wigglegram", icon="burst_mode"),
+                    ui_trigger=UiTrigger(show_button=False, title="Wigglegram", icon="burst_mode"),
                     gpio_trigger=GpioTrigger(pin="12"),
                     keyboard_trigger=KeyboardTrigger(keycode="w"),
                 ),
