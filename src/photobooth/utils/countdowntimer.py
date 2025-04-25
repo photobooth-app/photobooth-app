5c85a6721212cb3fc3c4e16a23b8df83bf4b5d0f
Michael G <me@mgineer85.de>
2025-04-25 21:55:00 +0200

    use timer, check if more accurate on macos.

--- src/photobooth/utils/countdowntimer.py
+++ src/photobooth/utils/countdowntimer.py
@@ -10,6 +10,7 @@ class CountdownTimer:
         self._lock = threading.Lock()
         self._done_event = threading.Event()
         self._thread = None
+        self._timer = None
         self._running = False
 
         self._duration: float = 0.0
@@ -24,21 +25,27 @@ class CountdownTimer:
             self._duration = duration
 
             logger.warning(time.perf_counter())
-
-            self._thread = threading.Thread(target=self._run_countdown, daemon=True)
-            self._thread.start()
+            self._timer = threading.Timer(duration, self._countdown_finished)
+            self._timer.start()
             logger.warning(time.perf_counter())
 
-    def _run_countdown(self):
+    def _countdown_finished(self):
         logger.warning(time.perf_counter())
-        time.sleep(self._duration)
-
-        logger.warning(time.perf_counter())
-
         with self._lock:
             self._running = False
             self._duration = 0.0
             self._done_event.set()
+
+    # def _run_countdown(self):
+    #     logger.warning(time.perf_counter())
+    #     time.sleep(self._duration)
+
+    #     logger.warning(time.perf_counter())
+
+    #     with self._lock:
+    #         self._running = False
+    #         self._duration = 0.0
+    #         self._done_event.set()
 
     def wait_countdown_finished(self):
         self._done_event.wait()
