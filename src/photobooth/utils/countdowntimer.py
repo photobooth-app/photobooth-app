9e3a5d128986b884ac399e61108064d00a1377b2
Michael G <me@mgineer85.de>
2025-04-25 22:02:43 +0200

    simplified timer because no ticks needed.

--- src/photobooth/utils/countdowntimer.py
+++ src/photobooth/utils/countdowntimer.py
@@ -1,6 +1,5 @@
 import logging
 import threading
-import time
 
 logger = logging.getLogger("counter")
 
@@ -9,9 +8,8 @@ class CountdownTimer:
     def __init__(self):
         self._lock = threading.Lock()
         self._done_event = threading.Event()
-        self._thread = None
-        self._timer = None
-        self._running = False
+        self._timer: threading.Timer | None = None
+        self._running: bool = False
 
         self._duration: float = 0.0
 
@@ -24,34 +22,14 @@ class CountdownTimer:
             self._running = True
             self._duration = duration
 
-            logger.warning(time.perf_counter())
             self._timer = threading.Timer(duration, self._countdown_finished)
             self._timer.start()
-            logger.warning(time.perf_counter())
 
     def _countdown_finished(self):
-        logger.warning(time.perf_counter())
         with self._lock:
             self._running = False
             self._duration = 0.0
             self._done_event.set()
 
-    # def _run_countdown(self):
-    #     logger.warning(time.perf_counter())
-    #     time.sleep(self._duration)
-
-    #     logger.warning(time.perf_counter())
-
-    #     with self._lock:
-    #         self._running = False
-    #         self._duration = 0.0
-    #         self._done_event.set()
-
     def wait_countdown_finished(self):
         self._done_event.wait()
-
-        logger.warning(time.perf_counter())
-
-    def is_running(self):
-        with self._lock:
-            return self._running
