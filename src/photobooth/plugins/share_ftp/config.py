0166d133cee17bb000684b6f6aa59899224c6854
Michael G <me@mgineer85.de>
2025-05-01 10:27:34 +0200

    ftp server plugin

--- src/photobooth/plugins/share_ftp/config.py
+++ src/photobooth/plugins/share_ftp/config.py
@@ -1,8 +1,49 @@
-from pydantic import Field
+from pydantic import Field, SecretStr, SerializationInfo, field_serializer
 from pydantic_settings import SettingsConfigDict
 
 from photobooth import CONFIG_PATH
 from photobooth.services.config.baseconfig import BaseConfig
+
+
+class Common(BaseConfig):
+    enabled: bool = Field(
+        default=False,
+        description="Enable integration to sync media files.",
+    )
+
+    share_url: str = Field(
+        default="http://test.de/{filename}",
+    )
+
+
+class FtpServer(BaseConfig):
+    host: str = Field(
+        default="",
+    )
+    port: int = Field(
+        default=21,
+    )
+    username: str = Field(
+        default="",
+    )
+    password: SecretStr = Field(
+        default=SecretStr(""),
+    )
+    secure: bool = Field(
+        default=True,
+    )
+    root_dir: str = Field(
+        default="/",
+    )
+
+    # reveal password in admin backend.
+    @field_serializer("password")
+    def contextual_serializer(self, value, info: SerializationInfo):
+        if info.context:
+            if info.context.get("secrets_is_allowed", False):
+                return value.get_secret_value()
+
+        return "************"
 
 
 class ShareFtpConfig(BaseConfig):
@@ -12,22 +53,5 @@ class ShareFtpConfig(BaseConfig):
         env_prefix="shareftp-",
     )
 
-    shareftp_enabled: bool = Field(
-        default=False,
-        description="Enable integration to sync photos.",
-    )
-    ftp_host: str = Field(
-        default="",
-    )
-    ftp_username: str = Field(
-        default="",
-    )
-    ftp_password: str = Field(
-        default="",
-    )
-    ftp_remote_dir: str = Field(
-        default="/",
-    )
-    share_url: str = Field(
-        default="http://test.de/{filename}",
-    )
+    common: Common = Common()
+    ftp_server: FtpServer = FtpServer()
--- src/photobooth/plugins/share_ftp/share_ftp.py
+++ src/photobooth/plugins/share_ftp/share_ftp.py
@@ -1,218 +1,373 @@
 import logging
-import os
 import threading
+from abc import ABC, abstractmethod
 from dataclasses import dataclass
-from ftplib import FTP_TLS, error_perm
+from ftplib import FTP_TLS, all_errors, error_perm
 from functools import lru_cache
+from itertools import count
 from pathlib import Path
-from queue import Empty, Queue
+from queue import Empty, PriorityQueue
 from typing import Literal
 
 from ...utils.resilientservice import ResilientService
 from .. import hookimpl
 from ..base_plugin import BasePlugin
-from .config import ShareFtpConfig
+from .config import FtpServer, ShareFtpConfig
 
 logger = logging.getLogger(__name__)
+lock = threading.Lock()
+counter = count()  # tie-breaker, always incr on put to queue so the following dataclass is not compared
 
 
 @dataclass
-class FtpTask:
-    cmd: Literal["upload", "delete"]
-    file: Path
+class SyncTaskUpload:
+    filepath_local: Path
+    filepath_remote: Path
 
 
-class ShareFtp(ResilientService, BasePlugin[ShareFtpConfig]):
+@dataclass
+class SyncTaskDelete:
+    filepath_remote: Path
+
+
+priorityQueueSyncType = PriorityQueue[tuple[int, int, SyncTaskUpload | SyncTaskDelete | None]]
+
+
+@lru_cache(maxsize=16)
+def get_folder_list_cached(_ftp: FTP_TLS, folder: Path) -> dict[str, dict[str, str]]:
+    # print(folder)
+    # out = {x[0]: int(x[1].get("size", 0)) for x in _ftp.mlsd(str(folder), ["size","type"])}
+    try:
+        out = {entry[0]: entry[1] for entry in _ftp.mlsd(str(folder), ["size", "type"])}
+    except error_perm as exc:
+        raise RuntimeError(f"FTP server does not support listing directory content, error: {exc}") from exc
+
+    return out
+
+
+def get_remote_filepath(local_root_dir: Path, local_filepath: Path) -> Path:
+    remote_path = local_filepath.relative_to(local_root_dir)
+    logger.info(f"{local_filepath} maps to {remote_path}")
+
+    return remote_path
+
+
+class BaseProtocolClient(ABC):
+    pass
+
+    @abstractmethod
+    def connect(self): ...
+    @abstractmethod
+    def disconnect(self): ...
+
+    @abstractmethod
+    def get_remote_filesize(self, filepath: Path) -> int | None: ...
+
+    @abstractmethod
+    def do_upload(self, local_path: Path, remote_path: Path): ...
+    @abstractmethod
+    def do_delete_remote(self, remote_path: Path): ...
+
+
+class FtpProtocolClient(BaseProtocolClient):
+    def __init__(self, config: FtpServer):
+        super().__init__()
+
+        self._root_dir: str = config.root_dir
+        self._host: str = config.host
+        self._port: int = config.port
+        self._username: str = config.username
+        self._password: str = config.password.get_secret_value()
+        self._secure: bool = config.secure
+
+        self._ftp: FTP_TLS | None = None
+
+        # None in queue can be used on shutdown to reduce waiting for timeout
+        # self._queue: Queue[SyncTaskUpload | SyncTaskDelete | None] = Queue()
+
+    def connect(self):
+        ret = []
+
+        if not self._host:
+            raise ValueError("no host given!")
+
+        # FTP_TLS-Verbindung aufbauen
+        self._ftp = FTP_TLS(timeout=5)
+
+        ret.append(self._ftp.connect(self._host, self._port))
+
+        if self._secure:
+            ret.append(self._ftp.auth())
+            ret.append(self._ftp.login(self._username, self._password))
+            ret.append(self._ftp.prot_p())
+        else:
+            ret.append(self._ftp.login(self._username, self._password))
+
+        ret.append(self._ftp.cwd(self._root_dir))
+
+        logger.info("FTP-Server Msg: " + "; ".join(ret))
+
+    def disconnect(self):
+        if self._ftp:
+            try:
+                ret = self._ftp.quit()
+                self._ftp = None
+                logger.debug("FTP-Server Msg: " + ret)
+            except all_errors as exc:
+                print(exc)
+                print("error disconn, but we just ignore this because on disc. failing quit means its disconnected from client perspective")
+
+    def get_folder_list(self, remote_path: Path):
+        folder_list = get_folder_list_cached(self._ftp, remote_path)
+        return folder_list
+
+    def get_remote_istype(self, filepath: Path, type: Literal["dir", "cdir", "pdir", "file"]) -> bool:
+        assert self._ftp
+
+        ftype = None
+
+        folder_list = self.get_folder_list(filepath.parent)
+
+        # if file is not found in the list, return None which means the file probably needs to be uploaded.
+        facts = folder_list.get(filepath.name, None)
+
+        if facts:
+            ftype = facts.get("type", None)
+
+        return ftype == type
+
+    def get_remote_filesize(self, filepath: Path) -> int | None:
+        assert self._ftp
+
+        size = None
+        out = None
+
+        folder_list = self.get_folder_list(filepath.parent)
+
+        # if file is not found in the list, return None which means the file probably needs to be uploaded.
+        facts = folder_list.get(filepath.name, None)
+
+        if facts:
+            size = facts.get("size", None)
+
+        if size:
+            out = int(size)
+
+        return out
+
+    def do_upload(self, local_path: Path, remote_path: Path):
+        assert self._ftp
+
+        # print(local_path)
+        # print(remote_path)
+
+        with lock:  # two clients *could* operate on the same folder in theory.
+            if not self.get_remote_istype(remote_path.parent, "dir"):
+                logger.debug(f"creating remote folder: {remote_path}")
+                try:
+                    self._ftp.mkd(str(remote_path.parent))
+                except Exception as exc:
+                    logger.error(f"error creating folder {remote_path}: {exc}")
+                    raise exc
+                else:
+                    get_folder_list_cached.cache_clear()
+
+        with open(local_path, "rb") as f:
+            self._ftp.storbinary(f"STOR {remote_path}", f)
+
+    def do_delete_remote(self, remote_path: Path):
+        assert self._ftp
+
+        self._ftp.delete(str(remote_path))
+
+
+class SyncWorker(ResilientService):
+    def __init__(self, sync_backend, queue: priorityQueueSyncType):
+        super().__init__()
+
+        self._sync_backend: BaseProtocolClient = sync_backend  # FTPClient, ... derived from BaseClient.
+        self._queue: priorityQueueSyncType = queue
+
+        self._idle_since_seconds: int = 0
+        self._idle_mode: bool = False
+
+    def start(self):
+        super().start()
+
+    def stop(self):
+        super().stop()
+
+    def setup_resource(self):
+        self._sync_backend.connect()
+
+    def teardown_resource(self):
+        self._sync_backend.disconnect()
+
+    def run_service(self):
+        assert self._sync_backend
+        assert self._queue
+        queue_timeout = 1  # wait until timout for a queue entry to process.
+        idle_mode_timeout = 30  # after timeout disconnect and be in idle mode.
+
+        while not self._stop_event.is_set():
+            # queue_size = self._queue.qsize()
+            # if queue_size > 0:
+            #     print(f"noch {queue_size} zu verarbeiten.")
+
+            try:
+                priotask = self._queue.get(timeout=queue_timeout)
+            except Empty:
+                if not self._idle_mode:
+                    self._idle_since_seconds += queue_timeout
+                    if self._idle_since_seconds >= idle_mode_timeout:
+                        logger.debug(f"No files to sync since {idle_mode_timeout}, disconnecting from server, waiting in idle mode")
+                        self._idle_mode = True
+                        self._sync_backend.disconnect()
+
+                continue
+            else:
+                self._idle_since_seconds = 0
+                task = priotask[2]
+
+                # quit on shutdown.
+                if task is None:
+                    break
+
+                if self._idle_mode:
+                    logger.debug("Resume from idle mode, connecting to server")
+                    self._sync_backend.connect()
+                    self._idle_mode = False
+
+                self._run_task(task)
+
+    def _run_task(self, task: SyncTaskUpload | SyncTaskDelete):
+        if isinstance(task, SyncTaskUpload):
+            self._sync_backend.do_upload(task.filepath_local, task.filepath_remote)
+        elif isinstance(task, SyncTaskDelete):
+            self._sync_backend.do_delete_remote(task.filepath_remote)
+        # else:
+        #     raise RuntimeError()
+        # TODO: what if task failed? reinsert to sync queue?
+
+        logger.info(f"sync job finished: {task}")
+
+
+# class RegularCompleteSync(ResilientService):
+#     def __init__(self, sync_backend, queue, local_root_dir: Path):
+#         super().__init__()
+
+#         self._sync_backend: BaseProtocolClient = sync_backend  # FTPClient, ... derived from BaseClient.
+#         self._queue: priorityQueueSyncType = queue
+#         self._local_root_dir: Path = local_root_dir
+
+#         # start resilient service activates below functions
+#         self.start()
+
+#     def start(self):
+#         super().start()
+
+#     def stop(self):
+#         super().stop()
+
+#     def setup_resource(self):
+#         self._sync_backend.connect()
+
+#     def teardown_resource(self):
+#         self._sync_backend.disconnect()
+
+#     def run_service(self):
+#         assert self._sync_backend
+#         assert self._queue
+
+#         while not self._stop_event.is_set():
+#             print("Starte Initial-datei sync...")
+
+#             for local_path in Path(self._local_root_dir).glob("**/*.*"):
+#                 # if self._stop_event.is_set():
+#                 #     print("stop queueuing because shutdown already requested.")
+#                 #     return
+
+#                 try:
+#                     remote_path = get_remote_filepath(self._local_root_dir, local_path)
+#                     size = self._sync_backend.get_remote_filesize(remote_path)
+#                     local_size = os.path.getsize(local_path)
+
+#                     if size != local_size:
+#                         self._queue.put_nowait((50, next(counter), SyncTaskUpload(local_path, remote_path)))
+#                         print(f"queueud for upload: {local_path}")
+
+#                 except Exception as e:
+#                     print(f"Fehler bei verarbeitung von {local_path}: {e}")
+#                     raise e
+
+#             print(get_folder_list_cached.cache_info())
+
+#             print("Initial-Synchronisation abgeschlossen.")
+
+#             # self.stop()
+#             # TODO: stop from within thread is not allowed (deadlock!) need to figure out a way to have a one-time living service.
+
+
+class ShareFtp(BasePlugin[ShareFtpConfig]):
     def __init__(self):
         super().__init__()
 
         self._config: ShareFtpConfig = ShareFtpConfig()
-        self._local_dir: Path = Path("./media/")
+        self._local_root_dir: Path = Path("./media/")
 
-        self._ftp: FTP_TLS | None = None
+        self._queue: priorityQueueSyncType = PriorityQueue()
 
-        self._service_ready: threading.Event = threading.Event()
+        self._sync_worker = None
 
-        # None in queue can be used on shutdown to reduce waiting for timeout
-        self._queue: Queue[FtpTask | None] = Queue()
+    def get_new_client(self):
+        # could instanciate different clients in future here...
+        return FtpProtocolClient(self._config.ftp_server)
 
     @hookimpl
     def start(self):
         """To start the resilient service"""
 
-        if not self._config.shareftp_enabled:
+        if not self._config.common.enabled:
             logger.info("Share FTP is disabled")
             return
 
-        super().start()
+        # start with fresh queue
+        self._queue: priorityQueueSyncType = PriorityQueue()
+
+        # consumes the queue and uploads/deletes/modifies remote filesystem according to the queue.
+        sync_client = self.get_new_client()
+        self._sync_worker = SyncWorker(sync_client, self._queue)
+        self._sync_worker.start()
 
     @hookimpl
     def stop(self):
         """To stop the resilient service"""
 
-        super().stop()
+        self._queue.put_nowait((0, next(counter), None))  # lowest (==highest) priority for None to stop processing.
 
-    def setup_resource(self):
-        # reset queue on start to avoid any leftovers...
-        self._queue = Queue()
+        if self._sync_worker:
+            self._sync_worker.stop()
 
-        # FTP_TLS-Verbindung aufbauen
-        self._ftp = FTP_TLS(self._config.ftp_host, timeout=5)
-        self._ftp.login(self._config.ftp_username, self._config.ftp_password)
-        self._ftp.prot_p()
-
-        self._ftp.cwd(self._config.ftp_remote_dir)
-
-    def teardown_resource(self):
-        self._service_ready.clear()
-
-        # clear queue on shutdown so None has immediate shutdown effect.
-        try:
-            while True:
-                self._queue.get_nowait()
-        except Empty:
-            pass
-
-        self._queue.put(None)
-
-        if self._ftp:
-            self._ftp.quit()
-
-    def wait_until_ready(self, timeout: float = 5) -> bool:
-        return self._service_ready.wait(timeout=timeout)
+    @hookimpl
+    def collection_original_file_added(self, files: list[Path]):
+        logger.info(f"SYNC File ORIGINAL ADDED NOTE {files}")
+        for file in files:
+            self._queue.put_nowait((20, next(counter), SyncTaskUpload(file, get_remote_filepath(self._local_root_dir, file))))
 
     @hookimpl
     def collection_files_added(self, files: list[Path]):
         logger.info(f"SYNC File added NOTE {files}")
         for file in files:
-            self._queue.put(FtpTask("upload", file))
+            self._queue.put_nowait((10, next(counter), SyncTaskUpload(file, get_remote_filepath(self._local_root_dir, file))))
 
     @hookimpl
     def collection_files_updated(self, files: list[Path]):
         logger.info(f"SYNC File updated NOTE {files}")
         for file in files:
-            self._queue.put(FtpTask("upload", file))
+            self._queue.put_nowait((10, next(counter), SyncTaskUpload(file, get_remote_filepath(self._local_root_dir, file))))
 
     @hookimpl
     def collection_files_deleted(self, files: list[Path]):
         logger.info(f"SYNC File delete NOTE {files}")
         for file in files:
-            self._queue.put(FtpTask("delete", file))
-
-    @lru_cache(maxsize=16)
-    def get_folder_files_sizes(self, folder: Path) -> dict[str, int]:
-        assert self._ftp
-        out = {x[0]: int(x[1].get("size", 0)) for x in self._ftp.mlsd(str(folder), ["size"])}
-
-        return out
-
-    def get_remote_filesize(self, filepath: Path) -> int | None:
-        assert self._ftp
-
-        sizes = self.get_folder_files_sizes(filepath.parent)
-
-        # if file is not found in the list, return None which means the file probably needs to be uploaded.
-        size = sizes.get(filepath.name, None)
-
-        return size
-
-    def run_service(self):
-        assert self._ftp
-
-        # Initial Sync durchführen
-        self._initial_sync_folderstructure()
-        self._initial_delta_queue()
-
-        self._service_ready.set()
-
-        while not self._stop_event.is_set():
-            queue_size = self._queue.qsize()
-            if queue_size > 0:
-                print(f"noch {queue_size} zu verarbeiten.")
-
-            try:
-                task = self._queue.get(timeout=1)
-            except Empty:
-                continue
-            else:
-                # quit on shutdown.
-                if task is None:
-                    break
-
-                self._run_ftp_task(task)
-
-    def _run_ftp_task(self, task: FtpTask):
-        assert self._ftp
-        if task.cmd == "upload":
-            self._ftp_upload(task.file)
-        elif task.cmd == "delete":
-            self._ftp_remote_delete_file(task.file)
-        # else not possible because of literal.
-
-        print(f"task done: {task}")
-
-    def _ftp_upload(self, local_path: Path):
-        assert self._ftp
-
-        remote_path = self._get_remote_filepath(local_path)
-
-        with open(local_path, "rb") as f:
-            self._ftp.storbinary(f"STOR {remote_path}", f)
-
-        print(f"hochgeladen: {remote_path}")
-
-    def _ftp_remote_delete_file(self, local_path: Path):
-        assert self._ftp
-        remote_path = self._get_remote_filepath(local_path)
-
-        self._ftp.delete(str(remote_path))
-
-        print(f"gelöscht: {remote_path}")
-
-    def _get_remote_filepath(self, local_filepath: Path) -> Path:
-        remote_path = Path(self._config.ftp_remote_dir, local_filepath.relative_to(self._local_dir))
-        # print(f"{local_filepath} maps to {remote_path}")
-
-        return remote_path
-
-    def _initial_sync_folderstructure(self):
-        assert self._ftp
-
-        print("Starte Initial-ordnerstruktur-sync...")
-        for local_path in Path(self._local_dir).glob("**/*"):
-            if not local_path.is_dir():
-                continue
-
-            try:
-                remote_path = self._get_remote_filepath(local_path)
-                self._ftp.mkd(str(remote_path))
-                print(f"Verzeichnis erstellt: {remote_path}")
-            except error_perm:
-                pass  # Existiert bereits
-                # ? was mit anderen fehlern?
-
-    def _initial_delta_queue(self):
-        assert self._ftp
-
-        print("Starte Initial-datei sync...")
-
-        for local_path in Path(self._local_dir).glob("**/*.*"):
-            if self._stop_event.is_set():
-                print("stop queueuing because shutdown already requested.")
-                return
-
-            try:
-                size = self.get_remote_filesize(self._get_remote_filepath(local_path))
-                local_size = os.path.getsize(local_path)
-
-                if size != local_size:
-                    self._queue.put(FtpTask("upload", local_path))
-                    print(f"queueud for upload: {local_path}")
-
-            except Exception as e:
-                print(f"Fehler bei verarbeitung von {local_path}: {e}")
-                raise e
-
-        print(self.get_folder_files_sizes.cache_info())
-
-        print("Initial-Synchronisation abgeschlossen.")
+            self._queue.put_nowait((15, next(counter), SyncTaskDelete(get_remote_filepath(self._local_root_dir, file))))
