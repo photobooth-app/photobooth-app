40cc56a5eaa28f1e37265d381641f11918fcbeaa
Michael G <me@mgineer85.de>
2025-05-12 07:26:13 +0200

    add a very basic test.

---
+++ src/tests/tests/plugins/test_sync.py
@@ -0,0 +1,75 @@
+import logging
+import threading
+import time
+from ftplib import FTP
+
+import pytest
+from pydantic import SecretStr
+from pyftpdlib.authorizers import DummyAuthorizer
+from pyftpdlib.handlers import FTPHandler
+from pyftpdlib.servers import ThreadedFTPServer
+
+from photobooth.plugins.share_ftp.config import Common, FtpServer, ShareFtpConfig
+from photobooth.plugins.share_ftp.share_ftp import ShareFtp
+
+logger = logging.getLogger(name=None)
+
+
+@pytest.fixture()
+def shrftp_plugin():
+    # setup
+    shrftp = ShareFtp()
+
+    shrftp._config = ShareFtpConfig(
+        common=Common(
+            enabled=True,
+        ),
+        ftp_server=FtpServer(
+            host="127.0.0.1",
+            port=2121,
+            username="testuser",
+            password=SecretStr("testpass"),
+            secure=False,
+            root_dir="/",
+        ),
+    )
+
+    yield shrftp
+
+
+@pytest.fixture()
+def ftp_server(tmp_path):
+    # Setup: configure and start the FTP server in a thread
+    authorizer = DummyAuthorizer()
+    authorizer.add_user("testuser", "testpass", homedir=tmp_path, perm="elradfmwT")
+
+    handler = FTPHandler
+    handler.authorizer = authorizer
+
+    server = ThreadedFTPServer(("127.0.0.1", 2121), handler)
+
+    thread = threading.Thread(target=server.serve_forever, kwargs={"handle_exit": False}, daemon=True)
+    thread.start()
+    # time.sleep(0.1)  # Allow server to start
+
+    yield  # Run the test
+
+    # Teardown
+    server.close_all()
+
+
+def test_ftp_login(ftp_server):
+    # just test the server itself is working fine...
+    ftp = FTP()
+    ftp.connect("127.0.0.1", 2121)
+    ftp.login("testuser", "testpass")
+    assert ftp.pwd() == "/"
+    ftp.quit()
+
+
+def test_init(ftp_server, shrftp_plugin: ShareFtp):
+    shrftp_plugin.start()
+
+    time.sleep(1)
+
+    shrftp_plugin.stop()
