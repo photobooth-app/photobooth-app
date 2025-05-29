3d1e2791c053594a51d9f33199cdb61051d28660
Michael G <me@mgineer85.de>
2025-05-29 19:36:49 +0200

    pilgram filter test update

--- src/tests/benchmarks/filter_pilgram2_test.py
+++ src/tests/benchmarks/filter_pilgram2_test.py
@@ -15,34 +15,28 @@ def filter_algo(request):
 
 def test_pilgram_lores_benchmark(benchmark, filter_algo):
     with Image.open("src/tests/assets/input_lores.jpg") as im:
-        if filter_algo == "__version__":
-            pytest.skip()
         benchmark(getattr(pilgram2, filter_algo), im)
 
 
-def test_pilgram_hires_benchmark(benchmark, filter_algo):
+# def test_pilgram_hires_benchmark(benchmark, filter_algo):
+#     with Image.open("src/tests/assets/input.jpg") as im:
+#         benchmark(getattr(pilgram2, filter_algo), im)
+
+
+def test_pilgram_memconsumption():
     with Image.open("src/tests/assets/input.jpg") as im:
-        if filter_algo == "__version__":
-            pytest.skip()
-        benchmark(getattr(pilgram2, filter_algo), im)
-
-
-def test_pilgram_memconsumption(filter_algo):
-    with Image.open("src/tests/assets/input.jpg") as im:
-        if filter_algo == "__version__":
-            pytest.skip()
-
         # Start tracing memory
         tracemalloc.start()
 
         # Benchmark the function
-        getattr(pilgram2, filter_algo)(im)
+        for fun in pilgram2.__all__:
+            getattr(pilgram2, fun)(im)
 
         # Get memory snapshot
-        current, peak = tracemalloc.get_traced_memory()
+        _, peak = tracemalloc.get_traced_memory()
         tracemalloc.stop()
 
         peak_mib = peak / (1024 * 1024)
-        logger.info(f"{filter_algo}: Peak memory usage = {peak_mib:.2f} MiB")
+        logger.info(f"Peak memory usage = {peak_mib:.2f} MiB")
 
         assert peak_mib < 300  # Set reasonable memory threshold
