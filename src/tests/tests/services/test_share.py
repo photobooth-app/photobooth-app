d57e5ae2c46d297cc5419689e485b333a69d096b
Michael G <me@mgineer85.de>
2025-09-17 18:31:24 +0200

    update tests for share actions

--- src/tests/tests/services/test_share.py
+++ src/tests/tests/services/test_share.py
@@ -1,14 +1,18 @@
 import logging
 import time
 from collections.abc import Generator
+from pathlib import Path
 from unittest.mock import patch
 
 import pytest
 
 from photobooth.appconfig import appconfig
 from photobooth.container import Container, container
+from photobooth.database.models import Mediaitem
+from photobooth.database.types import MediaitemTypes
 from photobooth.services.config.groups.share import ShareConfigurationSet, ShareProcessing
 from photobooth.services.config.models.trigger import Trigger
+from photobooth.utils.exceptions import WrongMediaTypeError
 
 logger = logging.getLogger(name=None)
 
@@ -31,6 +35,7 @@ def _container() -> Generator[Container, None, None]:
     appconfig.share.actions.append(
         ShareConfigurationSet(
             name="share_action_blocked_2",
+            handles_images_only=False,
             processing=ShareProcessing(share_command="", share_blocked_time=2, max_shares=0),
             trigger=Trigger(),
         )
@@ -38,7 +43,16 @@ def _container() -> Generator[Container, None, None]:
     appconfig.share.actions.append(
         ShareConfigurationSet(
             name="share_action_max_2",
+            handles_images_only=False,
             processing=ShareProcessing(share_command="", share_blocked_time=0, max_shares=2),
+            trigger=Trigger(),
+        )
+    )
+    appconfig.share.actions.append(
+        ShareConfigurationSet(
+            name="share_action_handles_images_only",
+            handles_images_only=True,
+            processing=ShareProcessing(share_command="", share_blocked_time=0, max_shares=0),
             trigger=Trigger(),
         )
     )
@@ -54,11 +68,8 @@ def test_print_service_disabled(mock_run, _container: Container):
 
     appconfig.share.sharing_enabled = False
 
-    # get the newest mediaitem
-    latest_mediaitem = _container.mediacollection_service.get_item_latest()
-
     with pytest.raises(ConnectionRefusedError):
-        _container.share_service.share(latest_mediaitem, 0)
+        _container.share_service.share(Mediaitem(media_type=MediaitemTypes.image, unprocessed=Path("1.jpg"), processed=Path("1.jpg")), 0)
 
     assert mock_run.assert_not_called
 
@@ -67,9 +78,7 @@ def test_print_service_disabled(mock_run, _container: Container):
 def test_print_image(mock_run, _container: Container):
     """enable service and try to print"""
 
-    # get the newest mediaitem
-    latest_mediaitem = _container.mediacollection_service.get_item_latest()
-    _container.share_service.share(latest_mediaitem, 0)
+    _container.share_service.share(Mediaitem(media_type=MediaitemTypes.image, unprocessed=Path("1.jpg"), processed=Path("1.jpg")), 0)
 
     # check subprocess.run was invoked
     mock_run.assert_called_once()
@@ -79,18 +88,14 @@ def test_print_image(mock_run, _container: Container):
 def test_print_image_blocked(mock_run, _container: Container):
     """enable service and try to print, check that it repsonds blocking correctly"""
 
-    # get the newest mediaitem
-    latest_mediaitem = _container.mediacollection_service.get_item_latest()
-
     # two prints issued
-
-    _container.share_service.share(latest_mediaitem, 1)
+    _container.share_service.share(Mediaitem(media_type=MediaitemTypes.image, unprocessed=Path("1.jpg"), processed=Path("1.jpg")), 1)
     time.sleep(2.5)
 
-    _container.share_service.share(latest_mediaitem, 1)
+    _container.share_service.share(Mediaitem(media_type=MediaitemTypes.image, unprocessed=Path("1.jpg"), processed=Path("1.jpg")), 1)
     time.sleep(1)
     with pytest.raises(BlockingIOError):
-        _container.share_service.share(latest_mediaitem, 1)
+        _container.share_service.share(Mediaitem(media_type=MediaitemTypes.image, unprocessed=Path("1.jpg"), processed=Path("1.jpg")), 1)
 
     # check subprocess.run was invoked
     assert mock_run.call_count == 2
@@ -104,9 +109,37 @@ def test_is_limited_exceeded(mock_run, _container: Container):
     for _ in range(appconfig.share.actions[test_action_index].processing.max_shares):
         _container.share_service.limit_counter_increment(appconfig.share.actions[test_action_index].name)
 
-    latest_mediaitem = _container.mediacollection_service.get_item_latest()
     with pytest.raises(BlockingIOError):
-        _container.share_service.share(latest_mediaitem, test_action_index)
+        _container.share_service.share(
+            Mediaitem(media_type=MediaitemTypes.image, unprocessed=Path("1.jpg"), processed=Path("1.jpg")), test_action_index
+        )
 
     # command was not called, means quota exceeded.
     assert mock_run.assert_not_called
+
+
+@patch("subprocess.run")
+def test_print_wrong_mediatype_raises(mock_run, _container: Container):
+    """enable service and try to print, check that it repsonds blocking correctly"""
+
+    # video fails for images only action
+    with pytest.raises(WrongMediaTypeError):
+        _container.share_service.share(Mediaitem(media_type=MediaitemTypes.video, unprocessed=Path("1.mp4"), processed=Path("1.mp4")), 3)
+    # animation
+    with pytest.raises(WrongMediaTypeError):
+        _container.share_service.share(Mediaitem(media_type=MediaitemTypes.animation, unprocessed=Path("1.gif"), processed=Path("1.gif")), 3)
+    # multicamera
+    with pytest.raises(WrongMediaTypeError):
+        _container.share_service.share(Mediaitem(media_type=MediaitemTypes.multicamera, unprocessed=Path("1.gif"), processed=Path("1.gif")), 3)
+
+    # check subprocess.run was not invoked
+    assert mock_run.call_count == 0
+
+    # image
+    _container.share_service.share(Mediaitem(media_type=MediaitemTypes.image, unprocessed=Path("1.jpg"), processed=Path("1.jpg")), 3)
+
+    # collage
+    _container.share_service.share(Mediaitem(media_type=MediaitemTypes.collage, unprocessed=Path("1.jpg"), processed=Path("1.jpg")), 3)
+
+    # check subprocess.run was not invoked
+    assert mock_run.call_count == 2
