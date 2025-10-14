f4a59bb7cbcdea51790a90778c98480fc8d66d36
Michael G <me@mgineer85.de>
2025-10-14 22:02:06 +0200

    exclude u2net for now.

--- src/photobooth/utils/rembg/sessions/__init__.py
+++ src/photobooth/utils/rembg/sessions/__init__.py
@@ -1,11 +1,12 @@
 from .base import BaseSession
 from .modnet import ModnetSession
-from .u2net import U2netSession
 from .u2netp import U2netpSession
+
+# from .u2net import U2netSession
 
 sessions: dict[str, type[BaseSession]] = {}
 
-sessions[U2netSession.name()] = U2netSession
+# sessions[U2netSession.name()] = U2netSession # not for now
 sessions[U2netpSession.name()] = U2netpSession
 sessions[ModnetSession.name()] = ModnetSession
 
