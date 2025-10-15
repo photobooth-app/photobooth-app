075111ada3ea3ec529237cc9500b8dc0868efebc
Michael G <me@mgineer85.de>
2025-10-15 12:51:08 +0200

    remove low quality AND slower models from lib.

--- src/photobooth/utils/rembg/sessions/__init__.py
+++ src/photobooth/utils/rembg/sessions/__init__.py
@@ -1,17 +1,13 @@
 from .base import BaseSession
 from .modnet import ModnetSession
-from .modnetfp16 import ModnetFp16Session
-from .modnetquantized import ModnetQuantizedSession
 from .u2net import U2netSession
 from .u2netp import U2netpSession
 
 sessions: dict[str, type[BaseSession]] = {}
 
+sessions[ModnetSession.name()] = ModnetSession
 sessions[U2netSession.name()] = U2netSession
 sessions[U2netpSession.name()] = U2netpSession
-sessions[ModnetSession.name()] = ModnetSession
-sessions[ModnetFp16Session.name()] = ModnetFp16Session
-sessions[ModnetQuantizedSession.name()] = ModnetQuantizedSession
 
 sessions_names = list(sessions.keys())
 sessions_class = list(sessions.values())
