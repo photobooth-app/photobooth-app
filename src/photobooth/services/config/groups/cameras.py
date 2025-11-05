e0530f5c5ec870ca73c6f8e8764ad77aaaf68ba0
Michael G <me@mgineer85.de>
2025-11-05 22:06:01 +0100

    move desc up so it is used as label in frontend automagically :)

--- src/photobooth/services/config/groups/cameras.py
+++ src/photobooth/services/config/groups/cameras.py
@@ -256,13 +256,13 @@ class WigglecamNodes(BaseModel):
     #     default=True,
     #     description="Enable node. Calibration might be invalid if chaning the nodes.",
     # )
-    device_id: int = Field(
-        default=0,
-        description="Unique ID of this node/device. Must match the device id configured in the wigglecam node.",
-    )
     description: str = Field(
         default="",
         description="Description just for you to distinguish the devices.",
+    )
+    device_id: int = Field(
+        default=0,
+        description="Unique ID of this node/device. Must match the device id configured in the wigglecam node.",
     )
     address: str = Field(
         default="0.0.0.0",
