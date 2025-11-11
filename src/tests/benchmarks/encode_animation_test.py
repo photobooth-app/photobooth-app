e208b16f7755f0aa4b081a8ad962eb16793d64a7
Michael G <me@mgineer85.de>
2025-11-11 18:51:33 +0100

    add mp4 encode animation for completeness.

--- src/tests/benchmarks/encode_animation_test.py
+++ src/tests/benchmarks/encode_animation_test.py
@@ -1,8 +1,11 @@
 import io
 import logging
+import tempfile
 from collections.abc import Generator
+from fractions import Fraction
 from typing import Any
 
+import av
 import pytest
 from PIL import Image
 
@@ -70,7 +73,7 @@ def pillow_encode_avif(images: list[Image.Image]):
         append_images=images[1:] if len(images) > 1 else [],
         quality=85,
         speed=10,
-        # lossless=False,
+        lossless=False,
         duration=125,
         loop=0,  # loop forever
     )
@@ -80,11 +83,64 @@ def pillow_encode_avif(images: list[Image.Image]):
     return bytes_full
 
 
+def pyav_encode_mp4(images: list[Image.Image], frame_duration: int = 125):
+    # ref https://github.com/PyAV-Org/PyAV/blob/main/examples/numpy/generate_video_with_pts.py
+    fps = round(1.0 / (frame_duration / 1000.0))  # frame_duration is in [ms], normize to [s] for pyav
+
+    n = len(images)
+    cycle = [*range(n), *range(n - 2, 0, -1)]
+    repeat = 1
+    sequence = cycle * repeat
+
+    in_img_w = images[0].width  # it is safe to assume all images have same dimensions
+    in_img_h = images[0].height
+
+    even_w = in_img_w if in_img_w % 2 == 0 else in_img_w - 1
+    even_h = in_img_h if in_img_h % 2 == 0 else in_img_h - 1
+    # set flag to crop. crop using PIL is more efficient than using reformatter to rescale by 1px.
+    need_crop = (even_w, even_h) != (in_img_w, in_img_h)
+
+    if need_crop:
+        for i, image in enumerate(images):
+            images[i] = image.crop(box=(0, 0, even_w, even_h))
+
+    with tempfile.NamedTemporaryFile(suffix=".mp4", delete=False) as tmp:
+        container = av.open(tmp.name, mode="w")
+        stream = container.add_stream("h264", rate=fps, options={"crf": "28", "preset": "veryfast"})  # crf lower is better quality
+        stream.codec_context.time_base = Fraction(1, fps)
+        stream.width = even_w
+        stream.height = even_h
+        # high compat. # TODO: does this automatically reformat if the input is different or just states it should be yuv420?
+        stream.pix_fmt = "yuv420p"
+
+        my_pts = 0  # [seconds]
+        for idx in sequence:
+            frame = av.VideoFrame.from_image(images[idx])
+            # frame = frame.reformat(format="yuv420p")# TODO: needed?
+            frame.pts = my_pts
+            my_pts += 1.0
+
+            for packet in stream.encode(frame):
+                container.mux(packet)
+
+        # last frame duplication seems not needed here, it plays correctly without
+
+        # Flush stream
+        for packet in stream.encode():
+            container.mux(packet)
+
+        # Close the file
+        container.close()
+
+        return tmp.read()  # bytes of the encoded file
+
+
 @pytest.fixture(
     params=[
         "pillow_encode_gif",
         "pillow_encode_webp",
         "pillow_encode_avif",
+        "pyav_encode_mp4",
     ]
 )
 def library(request):
@@ -93,6 +149,6 @@ def library(request):
 
 # needs pip install pytest-benchmark
 @pytest.mark.benchmark(group="encode_animation")
-def test_libraries_encode_hires(library, dummy_images, benchmark):
+def test_encode_animation(library, dummy_images, benchmark):
     benchmark(eval(library), images=dummy_images)
     assert True
