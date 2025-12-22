c5e2ec6ce3df0cf4e73edbb24228afc38987d7fb
Michael G <me@mgineer85.de>
2025-12-22 10:14:41 +0100

    update frontend.

---
+++ src/web/frontend/assets/private.use-form-0eevulTI.js
@@ -0,0 +1,1 @@
+import{c as n,A as r}from"./index-OPp8kL3S.js";const o={name:String};function s(e){return n(()=>({type:"hidden",name:e.name,value:e.modelValue}))}function c(e={}){return(t,u,a)=>{t[u](r("input",{class:"hidden"+(a||""),...e.value}))}}function i(e){return n(()=>e.name||e.for)}export{c as a,s as b,i as c,o as u};
