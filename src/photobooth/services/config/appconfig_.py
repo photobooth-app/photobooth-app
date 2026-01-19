e1bd268373cbf849e37e7113d0f433ba0df4bd2c
Michael G <me@mgineer85.de>
2026-01-19 19:28:01 +0100

    Revert "add filetransfer for last v8-stable release so users can transition to rclone slowly"
    This reverts commit 5d2e8478dbd08ba4ac073abfb69ca9c2c59083bb.

--- src/photobooth/services/config/appconfig_.py
+++ src/photobooth/services/config/appconfig_.py
@@ -14,7 +14,6 @@ from .baseconfig import BaseConfig
 from .groups.actions import GroupActions
 from .groups.cameras import GroupCameras
 from .groups.common import GroupCommon
-from .groups.filetransfer import GroupFileTransfer
 from .groups.hardwareinputoutput import GroupHardwareInputOutput
 from .groups.mediaprocessing import GroupMediaprocessing
 from .groups.misc import GroupMisc
@@ -48,7 +47,6 @@ class AppConfig(BaseConfig):
     actions: GroupActions = GroupActions()
     share: GroupShare = GroupShare()
     qrshare: GroupQrShare = GroupQrShare()
-    filetransfer: GroupFileTransfer = GroupFileTransfer()
     mediaprocessing: GroupMediaprocessing = GroupMediaprocessing()
     uisettings: GroupUiSettings = GroupUiSettings()
     backends: GroupCameras = GroupCameras()
