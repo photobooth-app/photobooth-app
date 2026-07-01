bb37d40602b705df4f8aa3cc3b23ade9e58359c8
Michael <michael@groene.de>
2026-07-01 11:05:33 +0200

    update tests

--- src/tests/benchmarks/encode_boomerang_mp4_test.py
+++ src/tests/benchmarks/encode_boomerang_mp4_test.py
@@ -5,16 +5,16 @@ import pytest
 
 from photobooth.services.mediaprocessing.context import VideoContext
 from photobooth.services.mediaprocessing.pipeline import Pipeline
-from photobooth.services.mediaprocessing.steps.video import BoomerangStep, BoomerangStepPyav
+from photobooth.services.mediaprocessing.steps.video import BoomerangStep, BoomerangStepFormerFfmpeg
 
 logger = logging.getLogger(name=None)
 
 
-def video_boomerang_stage():
+def video_boomerangFfmpeg_stage():
     video_in = Path("src/tests/assets/video.mp4")
 
     context = VideoContext(video_in)
-    steps = [BoomerangStep(1)]
+    steps = [BoomerangStepFormerFfmpeg(1)]
     pipeline = Pipeline[VideoContext](*steps)
     pipeline(context)
     assert context.video_processed
@@ -24,7 +24,7 @@ def video_boomerangPyav_stage():
     video_in = Path("src/tests/assets/video.mp4")
 
     context = VideoContext(video_in)
-    steps = [BoomerangStepPyav(1)]
+    steps = [BoomerangStep(1)]
     pipeline = Pipeline[VideoContext](*steps)
     pipeline(context)
     assert context.video_processed
@@ -32,7 +32,7 @@ def video_boomerangPyav_stage():
 
 @pytest.mark.benchmark(group="create_boomerang")
 def test_encode_boomerang_ffmpeg(benchmark):
-    benchmark(video_boomerang_stage)
+    benchmark(video_boomerangFfmpeg_stage)
 
 
 @pytest.mark.benchmark(group="create_boomerang")
--- src/tests/tests/mediaprocessing/test_videosteps.py
+++ src/tests/tests/mediaprocessing/test_videosteps.py
@@ -3,14 +3,13 @@ Testing Mediaprocessing collage
 """
 
 import logging
-import shutil
 from pathlib import Path
 
 import pytest
 
 from photobooth.services.mediaprocessing.context import VideoContext
 from photobooth.services.mediaprocessing.pipeline import Pipeline
-from photobooth.services.mediaprocessing.steps.video import BoomerangStep, BoomerangStepPyav
+from photobooth.services.mediaprocessing.steps.video import BoomerangStep, BoomerangStepFormerFfmpeg
 
 from ..util import video_duration
 
@@ -19,20 +18,20 @@ logger = logging.getLogger(name=None)
 
 def test_video_boomerang_stage():
     video_in = Path("src/tests/assets/video.mp4")
+    boomerang_speed = 3
 
     context = VideoContext(video_in)
-    steps = [BoomerangStep(1)]
+    steps = [BoomerangStep(boomerang_speed)]
     pipeline = Pipeline[VideoContext](*steps)
     pipeline(context)
     assert context.video_processed
     video_out = context.video_processed
-    # shutil.copy(video_out, "./boomerangstep.mp4")
 
     # boomerang reverses video so double length
     in_dur = video_duration(video_in)
     out_dur = video_duration(video_out)
 
-    assert out_dur == pytest.approx(in_dur * 2.0, abs=0.5)
+    assert out_dur == pytest.approx(in_dur * 2.0 * (1.0 / boomerang_speed), abs=0.5)
 
 
 def test_video_boomerangPyav_stage():
@@ -40,13 +39,11 @@ def test_video_boomerangPyav_stage():
     boomerang_speed = 3
 
     context = VideoContext(video_in)
-    steps = [BoomerangStepPyav(boomerang_speed)]
+    steps = [BoomerangStepFormerFfmpeg(boomerang_speed)]
     pipeline = Pipeline[VideoContext](*steps)
     pipeline(context)
     assert context.video_processed
     video_out = context.video_processed
-
-    shutil.copy(video_out, "./boomerangsteppyav.mp4")
 
     # boomerang reverses video so double length
     in_dur = video_duration(video_in)
